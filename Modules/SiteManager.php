a4156c496e34faf91d10aa48c1597243a24ec90a
Klaus Dimde <82110737+sunhill-de@users.noreply.github.com>
2022-04-01 12:37:29 +0200

    Update SiteManager.php

--- Modules/SiteManager.php
+++ Modules/SiteManager.php
@@ -5,5 +5,16 @@ namespace Sunhill\Visual\Modules;
 class SiteManager extends ModuleBase
 {
     
+    protected function addModuleGroup(string $name, $group)
+    {
+    }
+    
+    protected function addModule(string $name, string $group, $module)
+    {
+    }
+    
+    protected function addFeature(string $name, string $group, $feature)
+    {
+    }
     
 }
