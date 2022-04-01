d09cff79ce9553524513348b5ad05cd409cab9b3
Klaus Dimde <82110737+sunhill-de@users.noreply.github.com>
2022-04-01 12:34:38 +0200

    Update ModuleGroup.php

--- Modules/ModuleGroup.php
+++ Modules/ModuleGroup.php
@@ -5,5 +5,17 @@ namespace Sunhill\Visual\Modules;
 class ModuleGroup extends ModuleBase
 {
     
+    protected $site_manager;
     
+    public function setSiteManager(SiteManager $manager)
+    {
+        $this->site_manager = $manager;
+        return $this;
+    }
+    
+    public function getSiteManager(): String
+    {
+        return $this->site_manager;
+    }
+        
 }
