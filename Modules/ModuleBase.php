2ba50e5c95b1f41fb491a9e2e8c1cc9c00531888
Klaus Dimde <82110737+sunhill-de@users.noreply.github.com>
2022-04-01 12:31:13 +0200

    Added some getter and setter

--- Modules/ModuleBase.php
+++ Modules/ModuleBase.php
@@ -12,8 +12,42 @@ class ModuleBase
     
     protected $name;
     
-    public function __construct(string $name)
+    protected $icon;
+    
+    public function __construct()
     {
+        $this->setupModule();
+    }
+    
+    protected function setupModule()
+    {
+    }
+    
+    public function setName(string $name)
+    {    
         $this->name = $name;
+        return $this;
+    }
+    
+    public function getName() : String
+    {
+        return $this->name;
+    }
+    
+    /**
+     * Setter for the icon of this module
+     */
+    public function setIcon(string $name)
+    {
+        $this->icon = $name;
+        return $this;
+    }
+    
+    /**
+     * Getter for the icon of this module
+     */
+    public function getIcon(): String
+    {
+        return $this->icon;
     }
 }
