06407c077e6afbeebc3b6642e477ba7b0c556d8e
Klaus Dimde <82110737+sunhill-de@users.noreply.github.com>
2022-04-01 13:31:05 +0200

    Update SiteManager.php

--- Modules/SiteManager.php
+++ Modules/SiteManager.php
@@ -4,30 +4,11 @@ namespace Sunhill\Visual\Modules;
 
 class SiteManager extends ModuleBase
 {
-    protected $tree = [];
-    
-    /**
-     * Searches the tree for a module group with the given name
-     * It return a ModuleGroup object if found otherwise false
-     * @param $name string The name of the module group to search
-     */
-    protected function findModuleGroup(string $name)
+    protected function addModuleGroup(string $name, ModuleGroup $group)
     {
-        if (isset($this->tree[$name])) {
-            return $this->tree[$name];
-        } else {
-            return false;
-        }
-    }
-    
-    protected function addModuleGroup(string $name, $group)
-    {
-        if ($this->findModuleGroup($name)) {
-            throw new \Exception("The module group '$name' does already exist.");
-        } else {
-            $group->setSiteManager($this);
-            $this->tree[$name] = $group;           
-        }    
+        $group->setSiteManager($this);
+        $this->addSubEntity($name,$group);
+        return $this;
     }
     
     protected function addModule(string $name, string $group, $module)
