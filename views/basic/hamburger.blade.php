0e61621d7bda1bf37e1c8da37ad1f926ed121939
Klaus Dimde <82110737+sunhill-de@users.noreply.github.com>
2022-05-16 13:28:46 +0200

    Create hamburger.blade.php

---
+++ views/basic/hamburger.blade.php
@@ -0,0 +1,21 @@
+@extends('visual::basic.html')
+
+@section('body')
+<nav role='hamburger'>
+  <div id="menuToggle">
+    <input type="checkbox" />
+    <span></span>
+    <span></span>
+    <span></span>
+    <ul id="hamburger">
+      <a href="/"><li>Home</li></a>
+      @guest
+      <a href="/user/login"><li>Login</li></a>
+      @endguest
+      @auth
+      <a href="/user/logout"><li>Logout</li></a>
+      @endauth
+    </ul>
+  </div>
+</nav>
+@endsection
