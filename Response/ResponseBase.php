c87a18c7dd9e64e784033be4cf5e65b11b4d3e75
Klaus Dimde <82110737+sunhill-de@users.noreply.github.com>
2022-05-17 12:48:22 +0200

    Update ResponseBase.php

--- Response/ResponseBase.php
+++ Response/ResponseBase.php
@@ -10,6 +10,8 @@ abstract class ResponseBase
   
     protected $params;
   
+    protected $remaining;
+    
     public function setRequest(Request $request)
     {
       $this->request = $request;
@@ -22,6 +24,12 @@ abstract class ResponseBase
       return $this;
     }
   
+    public function setRemaining(string $remaining)
+    {
+        $this->remaining = $remaining;
+        return $this;
+    }
+    
     abstract protected function getResponse();
   
     protected function prepareResponse()
