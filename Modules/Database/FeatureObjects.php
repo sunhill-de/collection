7b23fa575990160606146b462a40f3f2308ef45a
Klaus Dimde <82110737+sunhill-de@users.noreply.github.com>
2022-05-31 09:56:49 +0200

    Implemented FeatureObjects

---
+++ Modules/Database/FeatureObjects.php
@@ -0,0 +1,35 @@
+<?php
+namespace Sunhill\Sunhill\Modules\Computer\Database;
+
+use Sunhill\Visual\Response\Database\ListObjectsResponse;
+use Sunhill\Visual\Response\Database\AddObjectResponse;
+use Sunhill\Visual\Response\Database\ExecAddObjectResponse;
+use Sunhill\Visual\Response\Database\EditObjectResponse;
+use Sunhill\Visual\Response\Database\ExecEditObjectResponse;
+use Sunhill\Visual\Response\Database\GroupEditObjectResponse;
+use Sunhill\Visual\Response\Database\ExecGroupEditObjectResponse;
+use Sunhill\Visual\Response\Database\DeleteObjectResponse;
+use Sunhill\Visual\Response\Database\ShowObjectResponse;
+
+use Sunhill\Visual\Modules\ModuleBase;
+
+class FeatureObjects extends ModuleBase
+{
+    
+    protected function setupModule()
+    {
+        $this->setName('Objects');        
+        $this->setDescription(__('Objects')); 
+        $this->addSubEntry('list', ListObjectsResponse::class);
+        $this->addSubEntry('add', AddObjectResponse::class);
+        $this->addSubEntry('execadd', ExecAddObjectResponse::class);
+        $this->addSubEntry('edit', EditObjectResponse::class);
+        $this->addSubEntry('execedit', ExecEditObjectResponse::class);
+        $this->addSubEntry('groupedit', GroupEditObjectResponse::class);
+        $this->addSubEntry('execgroupedit', ExecGroupEditObjectResponse::class);
+        $this->addSubEntry('delete', DeleteObjectResponse::class);
+        $this->addSubEntry('show', ShowObjectResponse::class);
+    }
+    
+    
+}
