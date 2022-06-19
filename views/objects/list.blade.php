8b3cf5e752603bb86153c8f3e4dea1766d5293a1
klaus dimde <klaus@dimde.info>
2022-06-19 20:02:31 +0200

    Implemented object listing

--- views/objects/list.blade.php
+++ views/objects/list.blade.php
@@ -2,17 +2,34 @@
 
 @section('title','Objekte auflisten')
 
-@section('body')
+@section('content')
 @parent
        <table>
-        <caption>Objekte von '{{$object}}' auflisten</caption>
+        <caption>Objekte von '{{$class}}' auflisten</caption>
         <tr>
-         <th>Name</th>
-         <th>Parent</th>
-         <th>&nbsp;</th>
+         <th>ID</th>
+         <th>Klasse</th>
+         @foreach ($columns as $col)
+         <th>{{ $col }}</th>
+         @endforeach
          <th>&nbsp;</th>
          <th>&nbsp;</th>
         </tr>
+        @forelse($objects as $object)
+        <tr>
+         <td>{{ $object->getID() }}</td>
+         <td>{{ $object::objectInfos['name'] }}</td>
+         @foreach ($columns as $col)
+         <td>{{ $object->$col }}</td>
+         @endforeach
+         <td><a href="{{ $prefix }}/Objects/edit">bearbeiten</a></td>
+         <td><a href="{{ $prefix }}/Objects/delete">l&ouml;schen</a></td>
+        </tr>
+		@empty
+		 <tr>
+		  <td colspan="5">No entries</td>
+		 </tr>
+        @endforelse
        </table>
        <a href="/">&Uuml;bersicht</a>
 @endsection
