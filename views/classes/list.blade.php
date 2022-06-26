64955aebda2e5b8b0afcd98fa2eddbf5a01a132d
Klaus Dimde <klaus@dimde.info>
2022-06-26 21:42:06 +0200

    Implemented ListClasses

--- views/classes/list.blade.php
+++ views/classes/list.blade.php
@@ -4,11 +4,13 @@
 
 @section('body')
 @parent
+       <div class="list">
        <table>
         <caption>Klassen auflisten</caption>
         <tr>
-         <th>Name</th>
-         <th>Parent</th>
+         <th><a href="{{ $prefix }}/Classes/list/{{ $delta }}/Name">Name</a></th>
+         <th><a href="{{ $prefix }}/Classes/list/{{ $delta }}/Parent">Parent</a></th>
+         <th>Beschreibung</th>
          <th>&nbsp;</th>
          <th>&nbsp;</th>
          <th>&nbsp;</th>
@@ -17,15 +19,16 @@
         <tr>
          <td>{{$class->name}}</td>
          <td>{{$class->parent}}</td>
-         <td><a href="/classes/show/{{$class->name}}">anzeigen</a></td>
-         <td><a href="/objects/list/{{$class->name}}">Objekte auflisten</a></td>
-         <td><a href="/objects/add/{{$class->name}}">Objekt hinzuf&uuml;gen</a></td>
+         <td>{{$class->description}}</td>
+         <td><a href="{{ $prefix }}/Classes/show/{{$class->name}}">anzeigen</a></td>
+         <td><a href="{{ $prefix }}/Objects/list/{{$class->name}}">Objekte auflisten</a></td>
+         <td><a href="{{ $prefix }}/Objects/add/{{$class->name}}">Objekt hinzuf&uuml;gen</a></td>
         </tr>
         @empty
         <tr>
          <td colspan="5">Keine Eintr&auml;ge</td>
         </tr> 
         @endforelse
-       </table>
+       </table></div>
        <a href="/">&Uuml;bersicht</a>
 @endsection
