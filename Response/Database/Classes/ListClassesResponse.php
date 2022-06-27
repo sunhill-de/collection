a97e4c0e00d2d50b3d030060e72bde4265c1c2b3
Klaus Dimde <82110737+sunhill-de@users.noreply.github.com>
2022-06-27 14:12:53 +0200

    Switch to ListResponse

--- Response/Database/Classes/ListClassesResponse.php
+++ Response/Database/Classes/ListClassesResponse.php
@@ -10,16 +10,12 @@ class ListClassesResponse extends BladeResponse
     
     protected $template = 'visual::classes.list';
     
-    private function sliceClassesList($classlist,$delta)
+    protected function prepareList($key=null)
     {
+        $all_classes = Classes::getAllClasses();
         $result = [];
-        $i = 0;
-        foreach ($classlist as $key => $description)
+        foreach ($all_classes as $name => $description)
         {
-            if ($i>($delta+1)*ENTRIES_PER_PAGE) {
-                return $result;
-            }
-            if ($i>=$delta*ENTRIES_PER_PAGE) {
                 $entry = new \StdClass();
                 $entry->name = $description['name'];
                 $entry->class = $description['class'];
@@ -27,34 +23,8 @@ class ListClassesResponse extends BladeResponse
                 $entry->description = (isset($description['description'])?$description['description']:"");
                 $entry->parent = $description['parent'];
                 $result[] = $entry;
-            }
-            $i++;
-        }
-        
+        }    
         return $result;
-    }
-
-    protected function getClassList(int $page, string $oder)
-    {
-        $all_classes = Classes::getAllClasses();
-        $classes = $this->sliceClassesList($all_classes,$page,ENTRIES_PER_PAGE);
-
-        $pages = [];
-        if (count($all_classes) > ENTRIES_PER_PAGE) {
-            $count = ceil(count($all_classes) / ENTRIES_PER_PAGE);
-            for ($i=0;$i<$count;$i++) {
-                $pages[$i] = $i*ENTRIES_PER_PAGE;
-            }
-        }
-        return $classes;
-    }
-    
-    protected function prepareResponse()
-    {
-        $passed = $this->solveRemaining('delta=0/order=name');        
-        $this->params['delta'] = $passed['delta'];
-        $this->params['order'] = $passed['order'];
-        $this->params['classes'] = $this->getClassList($passed['delta'],$passed['order']);        
     }
     
 }  
