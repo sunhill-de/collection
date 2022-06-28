c33a10078a40ae252bb5b9b8d878de79d910b5fb
Klaus Dimde <82110737+sunhill-de@users.noreply.github.com>
2022-06-28 14:40:12 +0200

    Create FeatureTags.php

---
+++ Modules/Database/FeatureTags.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Sunhill\Visual\Modules\Database;
+
+use Sunhill\Visual\Response\Database\ListTagsResponse;
+
+use Sunhill\Visual\Modules\ModuleBase;
+
+class FeatureTags extends ModuleBase
+{
+    
+    protected function setupModule()
+    {
+        $this->setName('Tags');        
+        $this->setDescription(__('Tags')); 
+        $this->addSubEntry('list', ListTagsResponse::class,__("list tags"));
+/*        $this->addSubEntry('add', AddObjectResponse::class);
+        $this->addSubEntry('execadd', ExecAddObjectResponse::class);
+        $this->addSubEntry('edit', EditObjectResponse::class);
+        $this->addSubEntry('execedit', ExecEditObjectResponse::class);
+        $this->addSubEntry('groupedit', GroupEditObjectResponse::class);
+        $this->addSubEntry('execgroupedit', ExecGroupEditObjectResponse::class);
+        $this->addSubEntry('delete', DeleteObjectResponse::class);
+        $this->addSubEntry('show', ShowObjectResponse::class); */
+    }
+    
+    
+}
