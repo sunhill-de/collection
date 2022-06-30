30f22546363ca15efa526f56826600049854bbd0
Klaus Dimde <klaus@dimde.info>
2022-06-30 23:16:31 +0200

    Corrected bugs

--- views/classes/list.blade.php
+++ views/classes/list.blade.php
@@ -1,4 +1,4 @@
-@extends('visual::basic.list')
+@extends('visual::basic.navigation')
 
 @section('title','Klassen auflisten')
 
@@ -6,20 +6,32 @@
 Klassen auflisten
 @endsection
 
-@section('headerrow')
-<th><a href="{{ $prefix }}/Classes/list/{{ $delta }}/Name">Name</a></th>
-<th><a href="{{ $prefix }}/Classes/list/{{ $delta }}/Parent">Parent</a></th>
-<th>Beschreibung</th>
-<th>&nbsp;</th>
-<th>&nbsp;</th>
-<th>&nbsp;</th>
-@endsection
-
-@section('datarow')
- <td>{{$class->name}}</td>
- <td>{{$class->parent}}</td>
- <td>{{$class->description}}</td>
- <td><a href="{{ $prefix }}/Classes/show/{{$class->name}}">anzeigen</a></td>
- <td><a href="{{ $prefix }}/Objects/list/{{$class->name}}">Objekte auflisten</a></td>
- <td><a href="{{ $prefix }}/Objects/add/{{$class->name}}">Objekt hinzuf&uuml;gen</a></td>
-@endsection
+@section('content')
+@parent
+<div class="list">
+ <table>
+  <caption>@yield('caption')</caption>
+  <tr>
+   <th><a href="{{ $prefix }}/Classes/list/{{ $delta }}/Name">Name</a></th>
+   <th><a href="{{ $prefix }}/Classes/list/{{ $delta }}/Parent">Parent</a></th>
+   <th>Beschreibung</th>
+   <th>&nbsp;</th>
+   <th>&nbsp;</th>
+   <th>&nbsp;</th>
+  </tr>
+  @forelse ($items as $item)
+  <tr>
+   <td>{{ $item->name }}</td>
+   <td>{{ $item->parent }}</td>
+   <td>{{ $item->description }}</td>
+   <td><a href="{{ $prefix }}/Classes/show/{{$item->name}}">anzeigen</a></td>
+   <td><a href="{{ $prefix }}/Objects/list/{{$item->name}}">Objekte auflisten</a></td>
+   <td><a href="{{ $prefix }}/Objects/add/{{$item->name}}">Objekt hinzuf&uuml;gen</a></td> 
+  </tr>
+  @empty
+  <tr>
+   <td colspan="100">Keine Eintr&auml;ge</td>
+  @endforelse
+ </table>
+</div>
+@endsection  
--- views/objects/list.blade.php
+++ views/objects/list.blade.php
@@ -1,28 +1,43 @@
-@extends('visual::basic.list')
+@extends('visual::basic.navigation')
 
 @section('title','Objekte auflisten')
 
 @section('caption')
-Objectke von '{{class}}' auflisten
+Objekte von '{{ $key }}' auflisten
 @endsection
 
-@section('headerrow')
- <th>ID</th>
- <th>Klasse</th>
- @foreach ($columns as $col)
- <th>{{ $col }}</th>
- @endforeach
- <th>&nbsp;</th>
- <th>&nbsp;</th>
-@endsection
-
-@section('datarow')
+@section('content')
+@parent
+<div class="list">
+ <table>
+  <caption>@yield('caption')</caption>
+  <tr>
+     <th>ID</th>
+     <th>Klasse</th>
+     @foreach ($columns as $col)
+     <th>{{ $col }}</th>
+     @endforeach
+     <th>&nbsp;</th>
+     <th>&nbsp;</th>
+  </tr>
+  @forelse ($items as $item)
+  <tr>
  <td>{{ $item->getID() }}</td>
  <td>{{ $item::objectInfos['name'] }}</td>
  @foreach ($columns as $col)
  <td>{{ $item->$col }}</td>
- @endforearch
+ @endforeach
  <td><a href="{{ $prefix }}/Objects/edit">bearbeiten</a></td>
  <td><a href="{{ $prefix }}/Objects/delete">l&ouml;schen</a></td>
-@endsection
+  </tr>
+  @empty
+  <tr>
+   <td colspan="100">Keine Eintr&auml;ge</td>
+  </tr>
+  @endforelse
+   
+</table>
+</div>
 
+@endsection
+  
