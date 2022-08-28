dd776c35a739e253c0a8444e640614aa0a1adf80
Klaus Dimde <klaus@dimde.info>
2022-08-28 21:31:26 +0200

    Returns a jquery compatible result

--- Controllers/AjaxController.php
+++ Controllers/AjaxController.php
@@ -3,15 +3,19 @@
 namespace Sunhill\Visual\Controllers;
 
 use Illuminate\Routing\Controller;
+use Sunhill\Visual\Facades\Dialogs;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+
+
+define('ANYWHERE', true);
+define('LIMIT', 10);
 
 class AjaxController extends Controller
 {
 
   public function searchTags(string $class="", Request $request,Response $response)
-  {
-  }
-  
-  public function searchObjects(string $class = "", $field = "", Request $request,Response $response)
   {
   }
   
@@ -24,4 +28,22 @@ class AjaxController extends Controller
         return response()->json($result,200)->header('Content-type', 'application/json');
   }
   
+  public function getArrayOfStringSuggestion(string $class, string $field)
+  {
+      
+  }
+  
+  public function searchObjects(string $class, string $field, Request $request)
+  {     
+      $search = $request->input('search');
+      $result = Dialogs::searchKeyfieldForField($class,$field,$search,ANYWHERE,LIMIT);
+      $newresult = [];
+      foreach ($result as $entry) {
+          $newentry = new \StdClass();
+          $newentry->label = $entry['keyfield'];
+          $newentry->id = $entry['id'];
+          $newresult[] = $newentry;
+      }
+      return $this->getOutput($newresult);      
+  }
 }
