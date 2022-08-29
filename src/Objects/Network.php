afb378d87da1a5fbd82d486116c8762f36a3e8eb
Klaus Dimde <82110737+sunhill-de@users.noreply.github.com>
2022-08-29 09:56:51 +0200

    Update Network.php

--- src/Objects/Network.php
+++ src/Objects/Network.php
@@ -4,8 +4,8 @@
  * @file Network.php
  * Providesthe Network object that describes a enclosed network
  * Lang en
- * Reviewstatus: 2022-03-18
- * Localization: unknown
+ * Reviewstatus: 2022-08-28
+ * Localization: complete
  * Documentation: unknown
  * Tests: unknown
  * Coverage: unknown
@@ -49,19 +49,32 @@ class Network extends ORMObject
             ->set_description('The network prefix (e.g. 192.168.3)')
             ->searchable()
             ->set_editable(true)
-            ->set_groupeditable(true)
+            ->set_groupeditable(false)
             ->set_displayable(true);
         self::varchar('descriptirion')
             ->set_description('A more verbose description of the network')
             ->searchable()
             ->set_editable(true)
-            ->set_groupeditable(true)
+            ->set_groupeditable(false)
             ->set_displayable(true);
         self::object('part_of')
             ->setAllowedObjects(['Network'])
             ->set_description('If this network is part of a larger network')
             ->set_displayable(true)
             ->set_editable(false)
-            ->set_groupeditable(false);
+            ->set_groupeditable(true);
     }
+    
+      protected static function setupInfos()
+	{
+		static::addInfo('name','Network');
+		static::addInfo('table','networks');
+       	static::addInfo('name_s','network',true);
+       	static::addInfo('name_p','networks',true);
+       	static::addInfo('description','Informations about a network');
+       	static::addInfo('options',0);
+		static::addInfo('editable',true);
+		static::addInfo('instantiable',true);
+	}
+
 }
