e2ed926f2cc07eb8ce3476c0c84eb9e760357f27
Klaus Dimde <82110737+sunhill-de@users.noreply.github.com>
2022-08-29 14:15:55 +0200

    +setupInfos() +Descriptions

--- src/Objects/Date.php
+++ src/Objects/Date.php
@@ -37,39 +37,56 @@ class Date extends ORMObject
     protected static function setupProperties()
     {
         self::varchar('name')
-        ->setMaxLen(100)
-        ->set_description('The name of the date')
-        ->set_displayable(true)
-        ->set_editable(true)
-        ->set_groupeditable(false)
-        ->searchable();
+            ->setMaxLen(100)
+            ->set_description('The name of the date')
+            ->set_displayable(true)
+            ->set_editable(true)
+            ->set_groupeditable(false)
+            ->searchable();
         self::date('begin_date')
-        ->searchable()
-        ->set_editable(true)
-        ->set_groupeditable(true)
-        ->set_displayable(true);
+            ->searchable()
+            ->set_description('The start date of the date')
+            ->set_editable(true)
+            ->set_groupeditable(true)
+            ->set_displayable(true);
         self::time('begin_time')
-        ->setDefault(null)
-        ->set_editable(true)
-        ->set_groupeditable(true)
-        ->set_displayable(true);
+            ->setDefault(null)
+            ->set_description('The start time of the date')
+            ->set_editable(true)
+            ->set_groupeditable(true)
+            ->set_displayable(true);
         self::date('end_date')
-        ->setDefault(null)
-        ->searchable()
-        ->set_editable(true)
-        ->set_groupeditable(true)
-        ->set_displayable(true);
+            ->setDefault(null)
+            ->set_description('The end date of the date')
+            ->searchable()
+            ->set_editable(true)
+            ->set_groupeditable(true)
+            ->set_displayable(true);
         self::time('end_time')
-        ->set_editable(true)
-        ->set_groupeditable(true)
-        ->set_displayable(true)
-        ->setDefault(null);
+            ->set_editable(true)
+            ->set_description('The end time of the date')
+            ->set_groupeditable(true)
+            ->set_displayable(true)
+            ->setDefault(null);
         self::arrayOfObjects('persons')
-        ->setAllowedObjects('Person')
-        ->set_editable(true)
-        ->set_groupeditable(true)
-        ->set_displayable(true)
-        ->setDefault(null)
-        ->searchable();
+            ->setAllowedObjects('Friend')
+            ->set_description('The involved persons')
+            ->set_editable(true)
+            ->set_groupeditable(true)
+            ->set_displayable(true)
+            ->setDefault(null)
+            ->searchable();
     }
+    
+    protected static function setupInfos()
+	{
+		static::addInfo('name','Date');
+		static::addInfo('table','dates');
+       	static::addInfo('name_s','date',true);
+       	static::addInfo('name_p','dates',true);
+       	static::addInfo('description','Informations about a date');
+       	static::addInfo('options',0);
+		static::addInfo('editable',true);
+		static::addInfo('instantiable',true);
+	}
 }
