970f089afb236008a934289e43e47bfd83d16ab8
Klaus Dimde <82110737+sunhill-de@users.noreply.github.com>
2022-08-30 14:58:57 +0200

    Create WrittenWork.php

---
+++ src/Objects/WrittenWork.php
@@ -0,0 +1,65 @@
+<?php
+
+/**
+ * @file WrittenWork.php
+ * Provides informations about written work 
+ * Lang en
+ * Reviewstatus: 2022-08-29
+ * Localization: complete
+ * Documentation: unknown
+ * Tests: unknown
+ * Coverage: unknown
+ * Dependencies: ORMObject
+ */
+namespace Sunhill\Objects\Objects;
+
+/**
+ * The class for written works
+ *
+ * @author lokal
+ *        
+ */
+class WrittenWork extends CreativeWork
+{
+    public static $table_name = 'writtenworks';
+    
+    public static $object_infos = [
+        'name'=>'WrittenWork',       // A repetition of static:$object_name @todo see above
+        'table'=>'writtenworks',     // A repitition of static:$table_name
+        'name_s' => 'written work',
+        'name_p' => 'written works',
+        'description' => 'Stores informations about written works',
+        'options'=>0,           // Reserved for later purposes
+    ];
+    
+    protected static function setupProperties()
+    {
+        parent::setupProperties();
+        self::enum('type')        
+            ->set_description('What type of work is this')
+            ->setEnumValues(['nonfiction','novel','shortstory','graphicnovel','other'])
+            ->searchable()
+            ->set_editable(true)
+            ->set_groupeditable(true)
+            ->set_displayable(true);
+        self::arrayOfObjects('authors')
+            ->setAllowedObject('person')
+            ->set_description('Who is/are the author(s)')
+            ->searchable()
+            ->set_displayable(true)
+            ->set_editable(true)
+            ->set_groupeditable(true); 
+    }
+    
+  protected static function setupInfos()
+	{
+		static::addInfo('name','WrittenWork');
+		static::addInfo('table','writtenworks');
+    static::addInfo('name_s','written work',true);
+    static::addInfo('name_p','written works',true);
+    static::addInfo('description','Stores informations about written works');
+    static::addInfo('options',0);
+		static::addInfo('editable',true);
+		static::addInfo('instantiable',true);
+	}
+}
