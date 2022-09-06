4dc86940a6378069dd8315ca490e24b7767a82a4
Klaus Dimde <82110737+sunhill-de@users.noreply.github.com>
2022-09-06 14:42:46 +0200

    add value_XXXX field

--- views/components/arrayofstrings.blade.php
+++ views/components/arrayofstrings.blade.php
@@ -1,11 +1,11 @@
 <div class="inputgroup">
  <fieldset>
  <legend>{{ __( $name ) }}</legend>
- <input type="text" name="__{{ $name }}" id="__{{ $name }}" />
+ <input type="text" name="input_{{ $name }}" id="input_{{ $name }}" />
+ <input type="hidden" name="value_{{ $name }}" id="value_{{ $name }}" /> <!-- only for compatibility -->
  <input type="button" value="+" onClick="addStrEntry( '{{ $name }}' )">
- <input type="button" value="-" onClick="delStrEntry( '{{ $name }}' )">
-
- <ul class="selectable" name="_{{ $name }}" id="_{{ $name }}">
+ 
+ <ul class="selectable" name="list_{{ $name }}" id="list_{{ $name }}">
   @isset($values)
   @foreach($values as $value)
  <li>{{ $value }}<input type="hidden" name="_{{ $name }}{{ $loop->index+1 }}" value="{{ $value }}"/></li>
