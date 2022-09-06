8d4655d8ec8a6b08bec041313e1c3d82196172c3
Klaus Dimde <82110737+sunhill-de@users.noreply.github.com>
2022-09-06 14:45:02 +0200

    normalized names

--- views/components/arrayofstrings.blade.php
+++ views/components/arrayofstrings.blade.php
@@ -3,17 +3,16 @@
  <legend>{{ __( $name ) }}</legend>
  <input type="text" name="input_{{ $name }}" id="input_{{ $name }}" />
  <input type="hidden" name="value_{{ $name }}" id="value_{{ $name }}" /> <!-- only for compatibility -->
- <input type="button" value="+" onClick="addStrEntry( '{{ $name }}' )">
+ <input type="button" value="+" onClick="addEntry( '{{ $name }}' )">
  
  <ul class="selectable" name="list_{{ $name }}" id="list_{{ $name }}">
   @isset($values)
   @foreach($values as $value)
- <li>{{ $value }}<input type="hidden" name="_{{ $name }}{{ $loop->index+1 }}" value="{{ $value }}"/></li>
+ <li>{{ $value }}<input type="hidden" name="value_{{ $name }}{{ $loop->index+1 }}" value="{{ $value }}"/></li>
   @endforeach
  @endisset
  </ul>
-
- <input type="hidden" name="_{{$name}}_count" id="_{{$name}}_count" value="@isset($values){{ count($values) }} @else 0 @endisset"/>
+ <input type="hidden" name="count_{{$name}}" id="count_{{$name}}" value="@isset($values){{ count($values) }} @else 0 @endisset"/>
  <script>
  	$( function() { stringArrayField('{{ $name }}', '{{ $class}}'); } );
  </script>
