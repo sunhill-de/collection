5b67e437b58efad852452eca73611e70dc659c40
Klaus Dimde <82110737+sunhill-de@users.noreply.github.com>
2022-09-08 15:22:27 +0200

    Update navigation.blade.php

--- views/basic/navigation.blade.php
+++ views/basic/navigation.blade.php
@@ -21,73 +21,8 @@
  
 @endforeach
 </style>
-<div class="mainnav" id="mainnav">
-<ul>
- <li><a id="home" href="/">Home</a></li>
-@isset($navigation)
- @foreach ($navigation as $level1_entry)
-  @if ($level1_entry->visible)
-  <li>
-    @if ($level1_entry->active)
-    <div class="active_navigation">
-    @endif
-    <a id="{{ $level1_entry->id }}" href="{{ $level1_entry->link }}">{{ $level1_entry->display_name }}</a>
-    @if ($level1_entry->subentries)
-     <!-- ******* Level 2 ******* -->
-     <ul>
-     @foreach ($level1_entry->subentries as $level2_entry)
-     <li>
-      @if ($level2_entry->active)
-      <div class="active_navigation">
-      @endif
-      <a id="{{ $level2_entry->id }}" href="{{ $level2_entry->link }}">{{ $level2_entry->display_name }}</a>
-      @if ($level2_entry->subentries)
-      <ul>
-      @foreach ($level2_entry->subentries as $level3_entry)
-      @if ($level1_entry->subentries)
-      <!-- ******* Level 3 ******* -->
-     <ul>
-     @foreach ($level1_entry->subentries as $level2_entry)
-     <li>
-      @if ($level2_entry->active)
-      <div class="active_navigation">
-      @endif
-      <a id="{{ $level2_entry->id }}" href="{{ $level2_entry->link }}">{{ $level2_entry->display_name }}</a>
-      @if ($level1_entry->subentries)
-      <ul>
-      @foreach ($level1_entry->subentries as $level2_entry)
-     
-      @endforeach
-      </ul>
-      @endif
-      @if ($level2_entry->active)
-      </div>
-      @endif
-    </li>      
-     @endforeach
-     </ul>
-     <!-- ******** Level 3 ******** -->
-    @endif
-     
-      @endforeach
-      </ul>
-      @endif
-      @if ($level2_entry->active)
-      </div>
-      @endif
-    </li>      
-     @endforeach
-     </ul>
-     <!-- ******** Level 2 ******** -->
-    @endif
-    @if ($level1_entry->active)
-    </div>
-    @endif
-  </li>   
-  @endif
- @endforeach
-@endisset
-</ul>
+
+@each('partials.navigation',$navigation,'entry')
 
 @if(null !== $breadcrumbs)
 <div id="breadcrumb">
