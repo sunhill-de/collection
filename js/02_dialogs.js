482bd1872ded1db25bf12fd9fdf5d06d06cd06eb
klaus dimde <klaus@dimde.info>
2022-09-10 21:58:52 +0200

    Fixed list adding

--- js/02_dialogs.js
+++ js/02_dialogs.js
@@ -55,15 +55,20 @@ function objectArrayField( id, classid ) {
  * @todo only do something, when there is an input (finished)
  * @todo clean the input field afterwards (finished)
  */
-function addEntry( id ) {
+function addEntry( id, valueonly ) {
     var entry_text = $( "#input_"+id ).val();  // Get the display value
     var entry_value = $( "#value_"+id ).val(); // Get the internal value	      
-    if ((entry_text) && (entry_value)) {
+    if ((entry_value) && (valueonly == true) ||
+        (valueonly == false) && (entry_text)) {
       var index = parseInt($('#count_'+id).val()) + 1; // Get the next index
 	  
       // Append it to the visual part
-      $('#list_'+id).append('<li>'+entry_text+'<input type="hidden" name="value_'+id+index+'" id="value_'+id+index+'" value="'+entry_value+'"/></li>');
-     // Append it to the hidden part
+      if (valueonly || entry_value) {
+	  	$('#list_'+id).append('<li>'+entry_text+'<input type="hidden" name="value_'+id+index+'" id="value_'+id+index+'" value="'+entry_value+'"/></li>');
+      } else {
+	  	$('#list_'+id).append('<li>'+entry_text+'<input type="hidden" name="value_'+id+index+'" id="value_'+id+index+'" value="'+entry_text+'"/></li>');	
+	  }
+	  // Append it to the hidden part
       $('#count_'+id).val(index);
       $( "#input_"+id ).val("");
       $( "#value_"+id ).val("");
