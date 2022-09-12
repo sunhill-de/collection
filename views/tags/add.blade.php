d4284875182ba269958cda3ad9c000d6aecf83d1
Klaus Dimde <82110737+sunhill-de@users.noreply.github.com>
2022-09-12 12:36:01 +0200

    Create add.blade.php

---
+++ views/tags/add.blade.php
@@ -0,0 +1,26 @@
+@extends('visual::basic.navigation')
+
+@section('title',__('Add tag'))
+
+@section('caption')
+{{ __('Add Tag') }}
+@endsection
+
+@section('content')
+@parent
+<form method="post" id="add" name="add" action="{{ $prefix }}/Tags/execadd">
+ <div class="dialogelements">
+ @csrf
+ <label for="tagname">{{ __('Tag name') }}</label>
+ <input name="name" type="text" />
+ <label for="parent">{{ __('Tag parent') }}</label>
+ <input name="input_parent" type="text" />
+ <inputgroup>
+  <caption>Optionen</caption>
+  <input type="checkbox" name="leafable" checked />{{ __('leafable') }}
+ </inputgroup>
+ <input type="submit" value="abschicken" />
+</div>
+</form>
+@endsection
+  
