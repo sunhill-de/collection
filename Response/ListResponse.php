0239109d9ce8e13a854ada4582067d20a10d02d1
Klaus Dimde <82110737+sunhill-de@users.noreply.github.com>
2022-09-12 15:22:53 +0200

    + paginator

--- Response/ListResponse.php
+++ Response/ListResponse.php
@@ -11,6 +11,11 @@ namespace Sunhill\Visual\Response;
 define("ENTRIES_PER_PAGE", 25);
 
 /**
+ * Defines how many paginator links should be left and right to the current entry
+ */
+define("PAGINATOR_NEIGHBOURS", 10);
+
+/**
  * Abstract response that represent a List of something
  * provides the necessary methods to enable sorting, slicing, etc. 
  */
@@ -76,8 +81,64 @@ abstract class ListResponse extends BladeResponse
         $this->params['items'] = $this->prepareMatrix($this->prepareList($this->params['key'],$this->params['order'],$this->params['delta'],ENTRIES_PER_PAGE));
     }
   
+    /**
+     * Returns the total number of entries in this list
+     * @return int
+     */
+    protected function getTotalEntryCount()
+    {
+    }
+    
+    protected function getPaginatorLink(int $index)
+    {
+    }
+    
+    protected function getCurrentPage()
+    {
+        return $this->params['delta'];
+    }
+    
     protected function processPaginator()
     {
+        if (ENTRIES_PER_PAGE >= $this->getTotalEntryCount()) {
+            $this->params['pages'] = [];
+            return;
+        }
+        $pages = ceil($this->getTotalEntryCount() / ENTRIES_PER_PAGE); // Number of pages
+        $current_page = $this->getCurrentPage();
+        
+        if (($current_page - PAGINATOR_NEIGHBOURS)<1) {
+            $start = 1;
+            $this->params['left_ellipse'] = '';            
+        } else {
+            $start = ($current_page - PAGINATOR_NEIGHBORS);
+            $this->params['left_ellipse'] = '...';                    
+        }    
+        if (($current_page + PAGINATOR_NEIGHBOURS)>($pages-1) {
+            $end = $pages - 1;
+            $this->params['right_ellipse'] = '';            
+        } else {
+            $end = ($current_page + PAGINATOR_NEIGHBOURS);
+            $this->params['right_ellipse'] = '...';            
+        }    
+            
+        $result = [];
+        $entry = new \StdClass();
+        $entry->link = $this->getPaginatorLink(0);
+        $entry->text = "1";
+        $result[] = $entry;
+        for ($i=$start;$i<$end;$i++) {
+            $entry = new \StdClass();
+            $entry->link = $this->getPaginatorLink($i);
+            $entry->text = $i+1;
+            $result[] = $entry;
+        }
+        $entry = new \StdClass();
+        $entry->link = $this->getPaginatorLink($pages-1);
+        $entry->text = $pages;
+        $result[] = $entry;
+        
+        $this->params['pages'] = $result;
     }
   
     /**
