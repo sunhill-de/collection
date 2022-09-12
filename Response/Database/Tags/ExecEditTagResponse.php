8c08f007eaf4b2319b2576798e2fa4b36abb751b
Klaus Dimde <82110737+sunhill-de@users.noreply.github.com>
2022-09-12 13:31:30 +0200

    Update ExecEditTagResponse.php

--- Response/Database/Tags/ExecEditTagResponse.php
+++ Response/Database/Tags/ExecEditTagResponse.php
@@ -11,6 +11,16 @@ class ExecEditTagResponse extends TagResponseBase
 {
     
     protected $target = '/';
-    
+   
+    protected function getWorkingTag()
+    {
+        $id = $this->solveRemaining('id');
+        
+        $tag = Tags::load($id);
+        $this->target = $this->getPrefix().'/Tags/show/'.$id;
+        
+        return $tag;
+    }
+
 }  
     
