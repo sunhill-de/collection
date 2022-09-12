3fd262ac6a109935d2899d913624b0875c557c05
Klaus Dimde <82110737+sunhill-de@users.noreply.github.com>
2022-09-12 12:54:48 +0200

    add parameters

--- Response/Database/Tags/EditTagResponse.php
+++ Response/Database/Tags/EditTagResponse.php
@@ -4,6 +4,7 @@ namespace Sunhill\Visual\Response\Database\Objects;
 
 use Sunhill\Visual\Response\BladeResponse;
 use Sunhill\ORM\Facades\Tags;
+use Sunhill\ORM\Objects\Tag;
 
 class EditTagResponse extends BladeResponse
 {
@@ -12,6 +13,17 @@ class EditTagResponse extends BladeResponse
         
     protected function prepareResponse()
     {
+        $result = $this->solveRemaining('id');
+        $tag_id = $result['id'];
+        $tag = Tags::load($tag_id);
+        
+        $this->params['title'] = __('Edit tag');
+        $this->params['action'] = $this->getPrefix().'/Tags/execedit';
+        
+        $this->params['name'] = $tag->name;
+        $this->params['parent_name'] = $tag->parent->fullpath;
+        $this->params['parent_id'] = $tag->parent->id;
+        $this->params['leafable'] = ($tag->options & TO_LEAFABLE) > 0;
     }
     
-}  
+}    
