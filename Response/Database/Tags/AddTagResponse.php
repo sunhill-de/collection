1b80caffcf004c7edbd0b4480c85263351aeeb76
Klaus Dimde <82110737+sunhill-de@users.noreply.github.com>
2022-09-12 12:48:30 +0200

    + parameters

--- Response/Database/Tags/AddTagResponse.php
+++ Response/Database/Tags/AddTagResponse.php
@@ -8,10 +8,16 @@ use Sunhill\ORM\Facades\Tags;
 class AddTagResponse extends BladeResponse
 {
 
-    protected $template = 'visual::tags.add';
+    protected $template = 'visual::tags.edit';
         
     protected function prepareResponse()
     {
+        $this->params['title'] = __('Add tag');
+        $this->params['action'] = $this->getPrefix().'/Tags/execadd';
+        $this->params['name'] = '';
+        $this->params['parent_name'] = '';
+        $this->params['parent_id'] = '';
+        $this->params['leafable'] = true;
     }
     
 }  
