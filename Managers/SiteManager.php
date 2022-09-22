66567142329664a8c61539365d069a0f8255d233
Klaus Dimde <82110737+sunhill-de@users.noreply.github.com>
2022-09-22 11:45:47 +0200

    Implemented addMainModule() and addSubModule()

--- Managers/SiteManager.php
+++ Managers/SiteManager.php
@@ -13,5 +13,33 @@ use Sunhill\Visual\Response\Database\Objects\AddObjectResponse;
 
 class SiteManager extends \Sunhill\Visual\Modules\SiteManager
 {
+        protected function getModule(string $name, $module=null)
+        {
+            if (is_a($module,ModuleBase::class)) {
+                $module->setName($name);
+                return $module;
+            } else if (is_null($module)) {
+                $result = new ModuleBase();
+                $result->setName($name);
+                return $result;
+            } else {
+                throw new \Exception("Can't handle the module");
+            }    
+        }
+    
+        public function addMainModule(string $name, $module = null)
+        {
+            $module = $this->getModule($name,$module);
+            return $this->addSubEntry($name,$module);
+        }
+    
+        public function addSubModule(string $main_module, string $sub_module_name, $submodule = null)
+        {
+            if (($module = $this->findSubEntry($main_module)) == false) {
+                $module = $this->addMainModule($main_module);
+            }
+            // $module points to the main module
+            return $module->addSubEntry($sub_module_name,$this->getModule($sub_module_name,$submodule));
+        }
     
 }
