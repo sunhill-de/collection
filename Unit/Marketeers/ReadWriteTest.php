68c226b8c3d30a256f064c828fc8557fbcb4431a
Klaus Dimde <82110737+sunhill-de@users.noreply.github.com>
2022-09-27 21:09:27 +0200

    Create ReadWriteTest.php

---
+++ Unit/Marketeers/ReadWriteTest.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace Sunhill\InfoMarket\Tests\Unit\Marketeers;
+
+use Sunhill\InfoMarket\Test\InfoMarketTestCase;
+use Sunhill\InfoMarket\Test\Marketeers\FakeMarketeer3;
+
+class ReadWriteTest extends InfoMarketTestCase
+{
+
+    /**
+     * @dataProvider ReadWriteProvider
+     * @param unknown $test
+     * @param unknown $offer
+     * @param unknown $expect
+     */
+    public function testRead($test,$isreadable,$value)
+    {
+        $test_obj = new FakeMarketeer3();
+        $this->assertEquals($isreadable,$test_obj->isReadable($test));
+        if ($isreadable) {
+          $this->assertEquals($value,$test_obj->getItem($test));
+        }
+    }
+    
+    public function ReadProvider()
+    {
+        return [
+          ['readable.byreadable',true,5],
+          ['readable.byget',true,5],
+          ['writeable.byset',false,null],
+        ];
+    }
+  
+    /**
+     * @dataProvider ReadWriteProvider
+     * @param unknown $test
+     * @param unknown $offer
+     * @param unknown $expect
+     */
+    public function testWrite($test,$iswriteable,$value)
+    {
+        $test_obj = new FakeMarketeer3();
+        $this->assertEquals($iswriteable,$test_obj->isWriteable($test));
+        if ($iswriteable) {
+          $test_obj->setItem($test,$value);
+          $this->assertEquals($value,$test_obj->getItem($test));
+        }
+    }
+    
+    public function WriteProvider()
+    {
+      return [    
+          ['writeable.bywriteable',true,7],
+          ['writeable.byset',true,7],
+          ['readable.byget',false,null],
+        ];
+    }
+    
+}
