6938229a6aa59e6d059edc9b44ae9d0d56105013
Klaus Dimde <klaus@dimde.info>
2022-09-28 22:59:20 +0200

    Fixed caching problem, removed obsolete tests

--- Unit/Marketeers/ReadWriteTest.php
+++ Unit/Marketeers/ReadWriteTest.php
@@ -4,6 +4,7 @@ namespace Sunhill\InfoMarket\Tests\Unit\Marketeers;
 
 use Sunhill\InfoMarket\Test\InfoMarketTestCase;
 use Sunhill\InfoMarket\Test\Marketeers\FakeMarketeer3;
+use Illuminate\Support\Facades\Cache;
 
 class ReadWriteTest extends InfoMarketTestCase
 {
@@ -16,6 +17,7 @@ class ReadWriteTest extends InfoMarketTestCase
      */
     public function testRead($test,$isreadable,$value)
     {
+        Cache::flush();
         $test_obj = new FakeMarketeer3();
         $this->assertEquals($isreadable,$test_obj->isReadable($test));
         if ($isreadable) {
@@ -29,6 +31,8 @@ class ReadWriteTest extends InfoMarketTestCase
           ['readable.byreadable',true,5],
           ['readable.byget',true,5],
           ['writeable.byset',false,null],
+          ['readwrite.byset',true,5],
+          ['readwrite.bywriteable',true,5],            
         ];
     }
   
@@ -40,12 +44,18 @@ class ReadWriteTest extends InfoMarketTestCase
      */
     public function testWrite($test,$iswriteable,$value)
     {
+        Cache::flush();
         $test_obj = new FakeMarketeer3();
         $this->assertEquals($iswriteable,$test_obj->isWriteable($test));
         if ($iswriteable) {
           $test_obj->setItem($test,$value);
-          $this->assertEquals($value,$test_obj->getItem($test)->getElement('value'));
-        }
+          if ($test_obj->isReadable($test)) {
+              $item = $test_obj->getItem($test);
+              $this->assertEquals($value,$item->getElement('value'));    
+          } else {
+              $this->assertEquals($value,$test_obj->value);
+          }          
+        } 
     }
     
     public function WriteProvider()
@@ -54,7 +64,9 @@ class ReadWriteTest extends InfoMarketTestCase
           ['writeable.bywriteable',true,7],
           ['writeable.byset',true,7],
           ['readable.byget',false,null],
-        ];
+          ['readwrite.byset',true,7],
+          ['readwrite.bywriteable',true,7],
+      ];
     }
     
 }
