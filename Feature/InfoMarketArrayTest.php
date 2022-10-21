d778fa9297e11185971966b864dde6e5d1344799
Klaus Dimde <klaus@dimde.info>
2022-10-21 20:07:06 +0200

    Add ArrayItemBase

---
+++ Feature/InfoMarketArrayTest.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace Sunhill\InfoMarket\Tests\Feature;
+
+use Sunhill\InfoMarket\Marketeers\MarketeerBase;
+use Sunhill\InfoMarket\Items\ArrayItemBase;
+use Sunhill\InfoMarket\Test\InfoMarketTestCase;
+use Sunhill\InfoMarket\Market\InfoMarket;
+
+class ArrayItem extends ArrayItemBase
+{
+    
+    protected function getArrayCount()
+    {
+        return 3;
+    }
+    
+    protected function getArrayItemByIndex(int $index)
+    {
+        return $index * 2;    
+    }
+    
+}
+
+class TestArrayMarketeer extends MarketeerBase
+{
+    
+    protected function getOffering(): array
+    {
+        return [
+            'test.item'=>ArrayItem::class,
+        ];
+    }
+    
+}
+
+class InfoMarketArrayTest extends InfoMarketTestCase
+{
+
+    protected function getMarket()
+    {
+        $test = new InfoMarket();
+        $test->installMarketeer(TestArrayMarketeer::class);
+        return $test;
+    }
+    
+    public function testReadArrayItemCount()
+    {
+        $test = $this->getMarket();
+        $this->assertEquals(3,$test->getItem('test.item.count','anybody','object')->value);
+    }
+    
+    public function testReadArrayItemIndex()
+    {
+        $test = $this->getMarket();
+        $this->assertEquals(10,$test->getItem('test.item.2','anybody','object')->value);        
+    }
+        
+}
