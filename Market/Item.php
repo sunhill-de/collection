81a20117bc59bf8eb14a5277ac23ca659ae20923
Klaus Dimde <klaus@dimde.info>
2022-10-31 19:52:07 +0100

    Adjusted methods and removed obsolte ones

--- Market/Item.php
+++ Market/Item.php
@@ -31,103 +31,85 @@ abstract class Item extends Leaf
     /**
      * The constructor overwrites the default metadata values with the ones 
      * defined for this item
+     * Test /tests/Unit/Market/ItemTest::testOverride()
      */
     public function __construct()
     {
         parent::__construct();
         // Overwrite the defaults (if necessary)
-        $this->default_metadata = $this->mergeMetadata($this->metadata);
-    }
-    
-    /**
-     * Items mustn't have further routing information, so raise an error
-     * {@inheritDoc}
-     * @see \Sunhill\InfoMarket\Market\Leaf::doRoute()
-     */
-    protected function doRoute(string $element, array $remains, string $credentials, Response &$response)
-    {
-        $response->error("TOOMUCHINFORMATION","Too much routing information");
-        return false;
-    }
-  
-    /**
-     * This is the expected end point for items.
-     * {@inheritDoc}
-     * @see \Sunhill\InfoMarket\Market\Leaf::routeFinished()
-     */
-    protected function routeFinished(string $credentials, Response &$response)
-    {
-        switch ($response->getElement('method')) {
-            case 'get':
-                return $this->getItem($credentials,$response);
-            case 'set':
-                return $this->setItem($response->getElement('value'),$credentials,$response);
-        }        
-    }
-    
-    /**
-     * Whenever the route ends here and the verb is "get" this routine is called. It checks the credentials
-     * collects the metadata and calls "doSetItemValue()" to get the value 
-     * @param $credentials string the current credentials of the user
-     * @param $response Response the current response object
-     */
-    protected function getItem(string $credentials, Response &$response)
-    {
-        if (!$this->isAllowedForReading($credentials, $response, [])) {
-            $response->error('USERNOTALLOWEDTOREAD',__("The current user ':credentials' is not allowed to read the item ':name'",['credentials'=>$credentials, 'name'=>$response->getElement('request')]));
-            return true;
-        }
-        $this->getMetadata($response);
-        if ($this->isReadable($response)) {
-            $response->value($this->doGetItemValue($response));
-        }
-        return true;
+        $this->default_metadata = $this->mergeMetadata($this->default_metadata,$this->metadata);
     }
 
-    /**
-     * Whenever the route ends here and the verb is set this routine is called. It checks the credentials
-     * collects the metadata, the previous value (if avaiable) and calls "doSetItemValue()" to change the 
-     * value
-     * @param $value The new value for this Item
-     * @param $credentials string the current credentials of the user
-     * @param $response Response the current response object
-     */
-    protected function setItem($value, string $credentials, Response &$response)
+    protected function isReadable($response, array $remains = [])
     {
-        if (!$this->isWriteable($response)) {
-            $response->error('ITEMNOTWRITEABLE',__("The item ':name' is not writeable",['name'=>$response->getElement('request')]));
-            return true;
-        }
-        if (!$this->isAllowedForWriting($credentials, $response)) {
-            $response->error('USERNOTALLOWEDTOWRITE',__("The current user ':credentials' is not allowed to write the item ':name'",['credentials'=>$credentials, 'name'=>$response->getElement('request')]));
-            return true;
-        }
-        $this->getMetadata($response);
-        // The response includes the previous value
-        if ($this->isReadable($response) && $this->isAllowedForWriting($credentials, $response)) {
-            $response->value($this->doGetItemValue($response));
-        }
-        $this->doSetItemValue($value, $response);        
-        return true;
+        return $this->default_metadata['readable'];
     }
     
-    /**
-     * Implemented as a dummy routine because there are items that are write only.
-     * Don't write directly into response, just return the value
-     */
-    protected function doGetItemValue(Response &$response)
+    protected function isWriteable($response, array $remains = [])
     {
-        return true;    
+        return $this->default_metadata['writeable'];
     }
     
-    /**
-     * Implemented as a dummy routine because there are items that are read only
-     */
-    protected function doSetItemValue($value, Response &$response)
+    protected function getReadRestriction(Response $response, array $remains = [])
     {
-        return false;
+        return $this->default_metadata['read_restriction'];
     }
     
+    protected function getWriteRestriction(Response $response, array $remains = [])
+    {
+        return $this->default_metadata['write_restriction'];
+    }
+    
+    protected function isThisAllowedToRead(string $credentials, array $remains = []): bool
+    {
+        return $this->checkRestriction($this->getReadRestriction($response, $remains),$credentials);
+    }
+    
+    protected function isThisAllowedToWrite(string $credentials, array $remains = []): bool
+    {
+        return $this->checkRestriction($this->getWriteRestriction($response, $remains),$credentials);
+    }
+    
+    protected function getUnit($response, $remains)
+    {
+        return $this->default_metadata['unit'];
+    }
+    
+    protected function getSemantic($response, $remains)
+    {
+        return $this->default_metadata['semantic'];
+    }
+    
+    protected function getType($response, $remains)
+    {
+        return $this->default_metadata['type'];
+    }
+    
+    protected function getUpdate($response, $remains)
+    {
+        return $this->default_metadata['update'];
+    }
+    
+    protected function getThisMetadata(Response &$response, array $remains = [])
+    {
+        $response
+        ->OK()
+        ->setElement('readable',$this->isReadable($response, $remains))
+        ->setElement('writeable',$this->isWriteable($response, $remains))
+        ->unit($this->getUnit($response, $remains))
+        ->semantic($this->getSemantic($response, $remains))
+        ->type($this->getType($response, $remains))
+        ->update($this->getUpdate($response, $remains));
+        if ($this->isReadable($response, $remains)) {
+            $response
+            ->setElement('read_restriction',$this->getReadRestriction($response, $remains));
+        }
+        if ($this->isWriteable($response, $remains)) {
+            $response
+            ->setElement('write_restriction',$this->getWriteRestriction($response, $remains));
+        }
+    }
+            
     /**
      * As an extension to the leaf function only return $this when $remains is empty
      * @param string $next
@@ -136,15 +118,10 @@ abstract class Item extends Leaf
     protected function getThisElement(string $next, array $remains)
     {
         if (empty($remains)) {
-            return parent::getElement($next, $remains);
+            return parent::getThisElement($next, $remains);
         } else {
             return null;
         }        
-    }
-    
-    public function getThisMetadata(Response &$response, array $remains = [] )
-    {
-        $this->getMetadata($response, $remains);
     }
     
     /**
@@ -152,50 +129,29 @@ abstract class Item extends Leaf
      * @param Response $response
      * @param array $remains
      */
-    protected function getThisValue(array $remains = [])
+    final protected function getThisValue(array $remains = [])
     {
-        // Ignore by default
+        if (empty($remains)) {
+            return $this->getItemValue($remains);
+        } else {
+            return null;
+        }
     }
-    
-    /**
-     * Wrapper for getThisValue
-     * @param array $remains
-     * @return unknown
-     */
-    public function getValue(array $remains = [])
-    {
-        return $this->getThisValue($remains);    
-    }
+
+    abstract protected function getItemValue();
     
     /**
      * Sets the value of this element or ignores the request if it's not possible to set a value
      * @param unknown $value
      * @param array $remains
      */
-    protected function setThisValue($value, array $remains = [])
+    final protected function setThisValue($value, array $remains = [])
     {
-        // Ignore by default
-    }
-    
-    /**
-     * Wrapper for setThisValue
-     * @param unknown $value
-     * @param array $remains
-     */
-    public function setValue($value, array $remains = [])
-    {
-        
-    }
-    
-    /**
-     * Returns if the current user is allowed to read this element
-     * @param string $credentials
-     * @param array $remains
-     * @return bool
-     */
-    public function isAllowedToRead(string $credentials, array $remains = []): bool
-    {
-        return true;
+        if (empty($remains)) {
+            return $this->setItemValue($value,$remains);            
+        } else {
+            return null;
+        }
     }
     
 }
