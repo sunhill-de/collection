1d3f94725251569cbee85aa8b215c38cc6999915
Klaus Dimde <klaus@dimde.info>
2022-11-04 22:03:02 +0100

    Implememted ObjectLeaf2

--- Market/ObjectLeaf.php
+++ Market/ObjectLeaf.php
@@ -2,7 +2,56 @@
 
 namespace Sunhill\InfoMarket\Market;
 
+use Sunhill\InfoMarket\Response\Response;
+
 class ObjectLeaf extends PseudoLeaf
 {
+    
+    protected function getSubroutedMetadata(string $first, Response &$response, array $remains)
+    {
+        
+    }
+        
+    protected function getObjectValue(string $first, array $remains)
+    {
+        // Overwrite in children    
+    }
+    
+    protected function getSubroutedValue(string $first, array $remains)
+    {
+        $result = $this->getObjectValue($first,$remains);
+        if (is_a($result,Element::class)) {
+            return $result->getValue($remains);
+        } else {
+            return $result;
+        }
+    }
+       
+    protected function setObjectValue(string $first, $value, array $remains)
+    {
+        
+    }
+    
+    protected function setSubroutedValue(string $first, $value, array $remains)
+    {
+        $result = $this->getObjectValue($first,$remains);
+        if (is_a($result,Element::class)) {
+            return $result->setValue($value,$remains);
+        } else {
+            return $this->setObjectValue($first, $value, $remains);
+        }        
+    }
+        
+    protected function isSubroutedAllowedToRead(string $first, string $credentials, array $remains): bool
+    {
+        
+    }
+    
+    
+    protected function isSubroutedAllowedToWrite(string $first, string $credentials, array $remains): bool
+    {
+        
+    }
+    
     
 }
