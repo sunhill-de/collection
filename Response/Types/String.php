52a00b4788a2867095b0886dc80b97e5c084cc23
Klaus Dimde <82110737+sunhill-de@users.noreply.github.com>
2022-11-08 11:30:05 +0100

    Create String.php

---
+++ Response/Types/String.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * @file String.php
+ * Represents an integer
+ * Lang en
+ * Reviewstatus: 2022-11-06
+ * Localization: none
+ * Documentation: complete
+ * Tests:
+ * Coverage: unknown
+ * Dependencies: none
+ * PSR-State: complete
+ */
+
+namespace Sunhill\InfoMarket\Response;
+
+class String extends Type
+{
+}
