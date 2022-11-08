f3542d0be606f466b90ff27361a6569ae9c0c796
Klaus Dimde <82110737+sunhill-de@users.noreply.github.com>
2022-11-08 11:37:29 +0100

    Create DateTime.php

---
+++ Response/Types/DateTime.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * @file DateTime.php
+ * Represents an datetime
+ * Lang en
+ * Reviewstatus: 2022-11-06
+ * Localization: none
+ * Documentation: complete
+ * Tests:
+ * Coverage: unknown
+ * Dependencies: none
+ * PSR-State: complete
+ */
+
+namespace Sunhill\InfoMarket\Response\Types;
+
+class DateTime extends Type
+{
+}
