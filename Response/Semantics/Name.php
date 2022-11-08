b718f0ce2851dba9244c667fd6a0fa0fea78b060
Klaus Dimde <82110737+sunhill-de@users.noreply.github.com>
2022-11-08 11:50:40 +0100

    Create Name.php

---
+++ Response/Semantics/Name.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * @file Name.php
+ * The semantic unit name is the name of an object or entity
+ * Lang en
+ * Reviewstatus: 2022-11-08
+ * Localization: none
+ * Documentation: complete
+ * Tests:
+ * Coverage: unknown
+ * Dependencies: none
+ * PSR-State: complete
+ */
+
+namespace Sunhill\InfoMarket\Response\Semantics;
+
+class Name extends Semantic
+{
+    // A name uses the default processing of Semantic    
+}
