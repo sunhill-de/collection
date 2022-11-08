12a4509e2e2edf57428f38dd3f09684b3e9ac04a
Klaus Dimde <82110737+sunhill-de@users.noreply.github.com>
2022-11-08 11:31:40 +0100

    Create Boolean.php

---
+++ Response/Types/Boolean.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * @file Boolean.php
+ * Represents an boolean
+ * Lang en
+ * Reviewstatus: 2022-11-06
+ * Localization: none
+ * Documentation: complete
+ * Tests:
+ * Coverage: unknown
+ * Dependencies: none
+ * PSR-State: complete
+ */
+
+namespace Sunhill\InfoMarket\Response\Types;
+
+class Boolean extends Type
+{
+}
