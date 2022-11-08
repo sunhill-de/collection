6636b9d33397ca274d9279e7308a02e801a1ac87
Klaus Dimde <82110737+sunhill-de@users.noreply.github.com>
2022-11-08 14:55:08 +0100

    Create InfoMarketTypesItem.php

---
+++ Marketeers/Internal/InfoMarketTypesItem.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Sunhill\InfoMarket\Marketeers\Internal;
+
+use Sunhill\InfoMarket\Response\Types\Type;
+
+class InfoMarketTypesItem extends InfoMarketArrayBase
+{
+  
+    function getBaseDir()
+    {
+      return dirname(__FILE__).'/../../Response/Types';
+    }  
+  
+    function classFits(string $test)
+    {
+      return is_a($test, Type::class, true);
+    }  
+
+}
