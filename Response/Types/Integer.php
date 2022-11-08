061f57f38f4eb3e782e870f701875241350cb25d
Klaus Dimde <82110737+sunhill-de@users.noreply.github.com>
2022-11-08 11:29:27 +0100

    Create Integer.php

---
+++ Response/Types/Integer.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * @file Integer.php
+ * Represents an integer
+ * Lang en
+ * Reviewstatus: 2022-11-06
+ * Localization: none
+ * Documentation: complete
+ * Tests:
+ * Coverage: unknown
+ * Dependencies: none
+ * PSR-State: complete
+ */
+
+namespace Sunhill\InfoMarket\Response;
+
+class Integer extends Type
+{
+}
