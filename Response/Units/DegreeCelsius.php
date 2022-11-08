2e6e767846172e777eea0a5792b667253cce830e
Klaus Dimde <82110737+sunhill-de@users.noreply.github.com>
2022-11-08 11:46:36 +0100

    Create DegreeCelsius.php

---
+++ Response/Units/DegreeCelsius.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * @file DegreeCelsius.php
+ * The unit °C
+ * Lang en
+ * Reviewstatus: 2022-11-06
+ * Localization: none
+ * Documentation: complete
+ * Tests:
+ * Coverage: unknown
+ * Dependencies: none
+ * PSR-State: complete
+ */
+
+namespace Sunhill\InfoMarket\Response\Units;
+
+class DegreeCelsius extends Unit
+{
+
+}
