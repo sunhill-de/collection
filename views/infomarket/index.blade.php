1a60d96eabbb5ca0e2f136613ea7cc4fc95b460b
Klaus Dimde <82110737+sunhill-de@users.noreply.github.com>
2022-11-15 11:50:33 +0100

    Layout of js

--- views/infomarket/index.blade.php
+++ views/infomarket/index.blade.php
@@ -10,55 +10,7 @@ Datenbank Hauptseite
 
 @section('content')
 @parent
-<script>
- $(function() {
-	$('.treeview').jstree({
-		"core": {
-		   "multiple": false,
-		   "animation": 0,
-           "themes": { "name":"default",
-               			"stripes":"true",
-                       "variant":"large",
-                       "dots":"true",
-                       "icons":"true" }
-		},
-		"plugins": [ "wholerow" ]
-	  }).on('changed.jstree', function (e,data) {
-			var id = data.instance.get_node(data.selected).id;
-			if (id.substring(0,5) == 'item:') {
-				id = id.substr(5);
-				$.ajax('/ajax/getItem/'+id,
-				{
-					dataType: 'json',
-					timeout: 500,
-					success: function ( data, status, xhr ) {
-						jsondata = JSON.parse( data );
-						console.log(jsondata);
-						$("#itemname").html(id);
-						$("#itemvalue").html( jsondata.value );
-						$("#itemunit").html( jsondata.unit );
-						$("#itemsemantic").html( jsondata.semantic );
-						$("#itemtype").html( jsondata.type );
-						$("#itemupdate").html (jsondata.update );
-						if (jsondata.writeable) {
-							$("#setvalue").enable();
-						} else {
-							$("#setvalue").disable();
-						}
-					}
-				})
-			}
-		  });
-	 
-	 	$("#setvalue").click( function() {
-	 		var id = $("#itemname").html();
-	 		alert(id); 
-	 	});
-	 });
-</script>
-<div class="treeview"><ul>
-@each('visual::partials.infomarket', $entries, 'entry')
-</ul></div>
+<div class="treeview"></div>
 <div id="info" class="footer">
  <div class="columns">
  <div class="column"><div class="label">{{ __("Item") }}:</div><div id="itemname"></div></div>
