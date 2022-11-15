145192ed272c665a638c26e03e1749f61d21c4d3
Klaus Dimde <82110737+sunhill-de@users.noreply.github.com>
2022-11-15 12:27:33 +0100

    implemented getNodes()

--- Controllers/AjaxController.php
+++ Controllers/AjaxController.php
@@ -19,5 +19,30 @@ class AjaxController extends Controller
       $result = InfoMarket::getItem($item);
       return json_encode($result);
   }
+ 
+  public function getNodes(string $parent, Request $request, Response $response)
+  {
+     $nodes = InfoMarket::getNodes($parent, 'object');
+     $result = [];
+     foreach ($nodes as $node) {
+        $node_data = [];
+        if ($parent == '#') {
+          $node_data['type'] = 'root';
+          $node_data['id'] = $node->name;
+        } else {
+          $node_data['id'] = $parent.'.'.$node->name;
+        }
+        $node_data['text'] = $node->name;
+        if ($node->semantic == 'Branch') {
+          $node_data["icon"] = "fa fa-folder icon-lg"
+          $node_data["children"] = true;
+        } else {
+          $node_data["icon"] = "fa fa-file fa-large kt-font-default";        
+          $node_data["children"] = false;
+        }  
+        $result[] = $node_data;
+     }  
+     return json_encode($result);
+  }
   
 }
