ffb57bdc2c308d671e5e6a228295b7bdc7b3aebd
Klaus Dimde <82110737+sunhill-de@users.noreply.github.com>
2023-01-03 14:46:16 +0100

    Reordered and added list and keyfields

--- src/ObjectsServiceProvider.php
+++ src/ObjectsServiceProvider.php
@@ -110,29 +110,49 @@ class ObjectsServiceProvider extends ServiceProvider
     protected function defineDialogs()
     {
         Dialogs::addObjectListFields(Anniversary::class,['name','type']);
+        Dialogs::addObjectKeyfield(Anniversary::class,':name');
+
         Dialogs::addObjectListFields(Country::class,['name','iso_code']);
+        Dialogs::addObjectKeyfield(Country::class,':name');
+
         Dialogs::addObjectListFields(CreativeWork::class,['name']);
+        Dialogs::addObjectKeyfield(CreativeWork::class,':name');
+        
+        Dialogs::addObjectListFields(Date::class,['begin_date','name']);
+        Dialogs::addObjectKeyfield(Date::class,':name');
+
         Dialogs::addObjectListFields(Event::class,['start_stamp','work'=>'work=>name']);
+        Dialogs::addObjectKeyfield(Event::class,':start_stamp');
+
+        Dialogs::addObjectListFields(Genre::class,['name','parent'=>'parent=>name']);
+        Dialogs::addObjectKeyfield(Genre::class,':name');
+
         Dialogs::addObjectListFields(Location::class,['name','part_of'=>'part_of=>name']);
+        Dialogs::addObjectKeyfield(Location::class,':name');
+
         Dialogs::addObjectListFields(Mime::class,['mimegroup','item']);
+        Dialogs::addObjectKeyfield(Mime::class,':mimegroup / :item');        
+
         Dialogs::addObjectListFields(MusicalArtist::class,['name','sort_name']);
+        Dialogs::addObjectKeyfield(MusicalArtist::class,':name');        
+        
+        Dialogs::addObjectListFields(Network::class,['name','prefix','description','part_of'=>'part_of=>name']);
+        Dialogs::addObjectKeyfield(Network::class,':name');
+
         Dialogs::addObjectListFields(Organisation::class,['name']);
+        Dialogs::addObjectKeyfield(Organisation::class,':name');
+
         Dialogs::addObjectListFields(Person::class,['firstname','lastname']);
+        Dialogs::addObjectKeyfield(Person::class,':firstname :lastname');
+        
         Dialogs::addObjectListFields(ProductGroup::class,['name','part_of'=>'part_of=>name']);
+        Dialogs::addObjectKeyfield(ProductGroup::class,':name');
+
+        Dialogs::addObjectListFields(Property::class,['name','type']);
+        Dialogs::addObjectKeyfield(Property::class,':name');
+
         Dialogs::addObjectListFields(Transaction::class,['order_id','shop'=>'shop=>name']);
         
-        Dialogs::addObjectKeyfield(Event::class,':start_stamp');
-        Dialogs::addObjectKeyfield(Country::class,':name');
-        Dialogs::addObjectKeyfield(CreativeWork::class,':name');
-        Dialogs::addObjectKeyfield(Location::class,':name');
-        Dialogs::addObjectKeyfield(Mime::class,':mimegroup / :item');        
-        Dialogs::addObjectKeyfield(MusicalArtist::class,':name');
-        Dialogs::addObjectKeyfield(Organisation::class,':name');
-        Dialogs::addObjectKeyfield(Person::class,':firstname :lastname');
-        Dialogs::addObjectKeyfield(Property::class,':name');
-        Dialogs::addObjectKeyfield(Genre::class,':name');
-        Dialogs::addObjectKeyfield(Network::class,':name');
-        Dialogs::addObjectKeyfield(Date::class,':name');
     }
     
     public function boot()
