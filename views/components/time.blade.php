0fe07b778769168c8b28060060acc5b39e7633c1
Klaus Dimde <82110737+sunhill-de@users.noreply.github.com>
2023-01-03 10:12:36 +0100

    Switch to bulma

--- views/components/time.blade.php
+++ views/components/time.blade.php
@@ -1,6 +1,6 @@
-<div class="inputgroup">
- <fieldset>
-  <legend>{{__($name)}}</legend>>
-  <input type="time" name="{{$name}}" id="{{$name}}" @isset($value)) value="{{ $value }}" @endisset/>
- </fieldset> 
+<div class="field">
+ <label class="label">{{__($name)}}</label>
+ <div class="control">
+  <input class="input" type="time" name="{{$name}}" id="{{$name}}" @isset($value)) value="{{ $value }}" @endisset/>
+ </div> 
 </div>
