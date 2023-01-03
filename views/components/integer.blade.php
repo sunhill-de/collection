4196f1d0cc93d155db5ca775cde3ffd9b8ea3f76
Klaus Dimde <82110737+sunhill-de@users.noreply.github.com>
2023-01-03 10:13:12 +0100

    switch to bulma: add missing class

--- views/components/integer.blade.php
+++ views/components/integer.blade.php
@@ -1,5 +1,5 @@
 <div class="field">
- <label>{{__($name)}}</label>>
+ <label class="label">{{__($name)}}</label>>
  <div class="control">
   <input class="input" type="number" name="{{$name}}" id="{{$name}}" @isset($value)) value="{{ $value }}" @endisset/>
  </div> 
