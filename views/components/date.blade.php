b362845a3a9ffb91935111a081ed15df753e1ce7
Klaus Dimde <82110737+sunhill-de@users.noreply.github.com>
2023-01-03 10:20:28 +0100

    Update date.blade.php

--- views/components/date.blade.php
+++ views/components/date.blade.php
@@ -1,6 +1,6 @@
-<div class="inputgroup">
- <fieldset>
-  <legend>{{__($name)}}</legend>>
-  <input type="date" name="{{$name}}" id="{{$name}}" @isset($value)) value="{{ $value }}" @endisset/>
- </fieldset> 
+<div class="field">
+ <label class="label">{{__($name)}}</label>
+ <div class="control"> 
+  <input class="input" type="date" name="{{$name}}" id="{{$name}}" @isset($value)) value="{{ $value }}" @endisset/>
+ </div> 
 </div>
