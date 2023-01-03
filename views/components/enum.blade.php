02b43b0502b83cdcf3d107153c4aa10e1d960fda
Klaus Dimde <82110737+sunhill-de@users.noreply.github.com>
2023-01-03 10:17:27 +0100

    Switch to bulma

--- views/components/enum.blade.php
+++ views/components/enum.blade.php
@@ -1,11 +1,13 @@
-<div class="inputgroup">
- <fieldset>
-  <legend>{{__($name)}}</legend>>
-  <select name="{{$name}}" id="{{$name}}">
-	<option value="">(leer)</option>
-	@foreach($entries as $value)
-	<option value="{{$value}}" @if (isset($selected) && ($selected == $value)) selected @endif>{{__($value)}}</option>
-	@endforeach
-  </select>
- </fieldset>
+<div class="field">
+ <label class="label">{{__($name)}}</label>
+ <div class="control">
+  <div class="select">
+   <select name="{{$name}}" id="{{$name}}">
+    <option value="">(leer)</option>
+    @foreach($entries as $value)
+    <option value="{{$value}}" @if (isset($selected) && ($selected == $value)) selected @endif>{{__($value)}}</option>
+    @endforeach
+   </select>
+  </div>
+ </div>
 </div>
