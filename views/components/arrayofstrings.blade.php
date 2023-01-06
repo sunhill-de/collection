97407a65e52e126c562ef552a63d87eacfd49476
Klaus Dimde <82110737+sunhill-de@users.noreply.github.com>
2023-01-06 13:32:45 +0100

    Switch to bulma and simplified fields

--- views/components/arrayofstrings.blade.php
+++ views/components/arrayofstrings.blade.php
@@ -1,21 +1,23 @@
-<div class="inputgroup">
- <fieldset>
- <legend>{{ __( $name ) }}</legend>
- <input type="text" name="input_{{ $name }}" id="input_{{ $name }}" />
- <input type="hidden" name="value_{{ $name }}" id="value_{{ $name }}" /> <!-- only for compatibility -->
- 
- <input type="button" value="+" onClick="addEntry( '{{ $name }}', false )">
- 
- <ul class="selectable" name="list_{{ $name }}" id="list_{{ $name }}">
+<div class="field">
+ <label class="label">{{ __( $name ) }}</label>
+ <div class="control">
+  <input class="input" type="text" name="input_{{ $name }}" id="input_{{ $name }}" />
+  <input type="hidden" name="value_{{ $name }}" id="value_{{ $name }}" /> <!-- only for compatibility -->
+  <div class="control">
+   <input class="button is-info" type="button" value="+" onClick="addEntry( 'tags', false )">
+  </div>
+  <div class="dynamic_list">
   @isset($values)
-  @foreach($values as $value)
- <li>{{ $value }}<input type="hidden" name="value_{{ $name }}{{ $loop->index+1 }}" value="{{ $value }}"/></li>
-  @endforeach
- @endisset
- </ul>
- <input type="hidden" name="count_{{$name}}" id="count_{{$name}}" value="@isset($values){{ count($values) }} @else 0 @endisset"/>
- <script>
+   @foreach($values as $value)
+   <div class="control">
+    <input class="input dynamic_entry" readonly name="{{ $name }}[]" />
+   </div>
+   @endforeach
+  @endisset
+  </div>
+ </div>
+</div>
+<script>
  	$( function() { stringArrayField('{{ $name }}', '{{ $class}}'); } );
- </script>
- </fieldset>	     
+</script>
 </div>
