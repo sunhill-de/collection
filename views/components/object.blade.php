1d1930bd1f4a1538527cc53b8a5a40930f9035dc
Klaus Dimde <klaus@dimde.info>
2023-01-06 21:07:00 +0100

    Restyled and switched to bulma

--- views/components/object.blade.php
+++ views/components/object.blade.php
@@ -1,12 +1,11 @@
-<div class="inputgroup">
- <fieldset>
+<div class="field">
+ <label class="label">{{ __( $name ) }}</label>
 
- <input name="input_{{ $name }}" id="input_{{ $name }}"  @isset($key) value="{{ $obj_key }}" @endisset />
+ <div class="control">
+  <input  class="input is-small" style="width: 33%;" name="input_{{ $name }}" id="input_{{ $name }}"  @isset($key) value="{{ $obj_key }}" @endisset />
+ </div>
  <input type="hidden" name="value_{{ $name }}" id="value_{{ $name }}" @isset($id) value="{{ $obj_id }}" @endisset/>
-
- <legend>{{ __( $name ) }}</legend>
  <script>
  	$( function() { objectField('{{ $name }}', '{{ $class}}'); } );
  </script>
- </fieldset>	     
 </div>
