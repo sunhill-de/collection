1ad298a8deb2c425fad60f10679becbc1b826220
Klaus Dimde <82110737+sunhill-de@users.noreply.github.com>
2023-01-09 13:13:28 +0100

    Update arrayofstrings.blade.php

--- views/components/arrayofstrings.blade.php
+++ views/components/arrayofstrings.blade.php
@@ -1,23 +1,31 @@
 <div class="field">
  <label class="label">{{ __( $name ) }}</label>
- <div class="control">
-  <input class="input" type="text" name="input_{{ $name }}" id="input_{{ $name }}" />
-  <input type="hidden" name="value_{{ $name }}" id="value_{{ $name }}" /> <!-- only for compatibility -->
-  <div class="control">
-   <input class="button is-info" type="button" value="+" onClick="addEntry( 'tags', false )">
+ <div class="columns">
+  <div class="column">
+   <label class="label">{{__( "Search" ) }}</label>
+   <div class="control">
+    <input class="input" type="text" name="input_{{ $name }}" id="input_{{ $name }}" />
+    <input type="hidden" name="value_{{ $name }}" id="value_{{ $name }}" /> <!-- only for compatibility -->
+    <div class="control">
+     <input class="button is-info" type="button" value="+" onClick="addEntry( 'tags', false )">
+    </div>
   </div>
+  
+  <div class="column">
+   <label class="label">{{__( "Current setting" ) }}</label>  
   <div class="dynamic_list">
-  @isset($values)
+   @isset($values)
    @foreach($values as $value)
    <div class="control">
     <input class="input dynamic_entry" readonly name="{{ $name }}[]" />
    </div>
    @endforeach
-  @endisset
+   @endisset
+   </div>
   </div>
  </div>
 </div>
+
 <script>
  	$( function() { stringArrayField('{{ $name }}', '{{ $class}}'); } );
 </script>
-</div>
