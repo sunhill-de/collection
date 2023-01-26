fafb682d9b407acc9027a197c256d31fbfb43c36
Klaus Dimde <klaus@dimde.info>
2023-01-26 17:26:11 +0100

    Copied from old ListResponse and adjusted to new response

--- Response/Database/Classes/ListClassesResponse.php
+++ Response/Database/Classes/ListClassesResponse.php
@@ -2,10 +2,10 @@
 
 namespace Sunhill\Visual\Response\Database\Classes;
 
-use Sunhill\Visual\Response\ListResponse;
+use Sunhill\Visual\Response\SunhillListResponse;
 use Sunhill\ORM\Facades\Classes;
 
-class ListClassesResponse extends ListResponse
+class ListClassesResponse extends SunhillListResponse
 {
     
     protected $template = 'visual::classes.list';
@@ -13,13 +13,13 @@ class ListClassesResponse extends ListResponse
     protected function prepareHeaders(): array 
     {
         $this->params['headers'] = [
-            $this->createStdClass(['name'=>__('Class name'),'link'=>null]),
-            $this->createStdClass(['name'=>__('Name'),'link'=>null]),
-            $this->createStdClass(['name'=>__('Description'),'link'=>null]),
-            $this->createStdClass(['name'=>__('Parent'),'link'=>null]),
-            $this->createStdClass(['name'=>"",'link'=>null]),
-            $this->createStdClass(['name'=>"",'link'=>null]),
-            $this->createStdClass(['name'=>"",'link'=>null]),
+            $this->getStdClass(['name'=>__('Class name'),'link'=>null]),
+            $this->getStdClass(['name'=>__('Name'),'link'=>null]),
+            $this->getStdClass(['name'=>__('Description'),'link'=>null]),
+            $this->getStdClass(['name'=>__('Parent'),'link'=>null]),
+            $this->getStdClass(['name'=>"",'link'=>null]),
+            $this->getStdClass(['name'=>"",'link'=>null]),
+            $this->getStdClass(['name'=>"",'link'=>null]),
         ];
         return $this->params['headers'];
     }
@@ -29,19 +29,24 @@ class ListClassesResponse extends ListResponse
         return $this->sliceList(Classes::getAllClasses(),$delta);
     }
 
+    protected function getPrefix()
+    {
+        return '/Computer/Database';    
+    }
+    
     protected function prepareMatrix($input): array
     {
         $result = [];        
         foreach ($input as $name => $description)
         {
             $row = [];
-            $row[] = $this->createStdClass(['name'=>$description['class'],'link'=>null]);
-            $row[] = $this->createStdClass(['name'=>$description['name'],'link'=>null]);
-            $row[] = $this->createStdClass(['name'=>(isset($description['description'])?$description['description']:""),'link'=>null]);
-            $row[] = $this->createStdClass(['name'=>$description['parent'],'link'=>null]);
-            $row[] = $this->createStdClass(['name'=>__('list'),'link'=>$this->params['prefix'].'/Objects/list/'.$description['name']]);
-            $row[] = $this->createStdClass(['name'=>__('add'),'link'=>$this->params['prefix'].'/Objects/add/'.$description['name']]);
-            $row[] = $this->createStdClass(['name'=>__('show'),'link'=>$this->params['prefix'].'/Objects/show/'.$description['name']]);
+            $row[] = $this->getStdClass(['name'=>$description['class'],'link'=>null]);
+            $row[] = $this->getStdClass(['name'=>$description['name'],'link'=>null]);
+            $row[] = $this->getStdClass(['name'=>(isset($description['description'])?$description['description']:""),'link'=>null]);
+            $row[] = $this->getStdClass(['name'=>$description['parent'],'link'=>null]);
+            $row[] = $this->getStdClass(['name'=>__('list'),'link'=>$this->getPrefix().'/Objects/list/'.$description['name']]);
+            $row[] = $this->getStdClass(['name'=>__('add'),'link'=>$this->getPrefix().'/Objects/add/'.$description['name']]);
+            $row[] = $this->getStdClass(['name'=>__('show'),'link'=>$this->getPrefix().'/Objects/show/'.$description['name']]);
             $result[] = $row;
         }
         return $result;        
@@ -55,7 +60,7 @@ class ListClassesResponse extends ListResponse
     
     protected function getPaginatorLink(int $index)
     {
-        return $this->params['prefix'].'/Classes/list/'.$index;
+        return $this->getPrefix().'/Classes/List/'.$index;
     }
     
 }  
