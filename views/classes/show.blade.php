40dbf77d7cc6a5a91ff363ee4810a2ce75bea098
Klaus Dimde <klaus@dimde.info>
2023-01-26 17:54:10 +0100

    Prepared implementation of ShowClass()

---
+++ views/classes/show.blade.php
@@ -0,0 +1,13 @@
+@extends('visual::basic.navigation')
+
+@section('title',__('Show class'))
+
+@section('caption')
+{{ __('Show class') }}
+@endsection
+
+@section('content')
+@parent
+Zeigt eine Klasse an
+@endsection
+  
