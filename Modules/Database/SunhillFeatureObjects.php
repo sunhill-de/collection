278311dab1744b70e51b349c8fe49b4e53de1995
Klaus Dimde <klaus@dimde.info>
2023-01-29 21:10:48 +0100

    Added missing feature actions

--- Modules/Database/SunhillFeatureObjects.php
+++ Modules/Database/SunhillFeatureObjects.php
@@ -23,6 +23,27 @@ class SunhillFeatureObjects extends SunhillModuleBase
             ->addControllerAction([\Sunhill\Visual\Controllers\Database\ObjectsController::class, 'show'])
             ->setVisible(false)
             ->setRouteAddition('/{id}');
+        $this->addAction('Add')
+            ->addControllerAction([\Sunhill\Visual\Controllers\Database\ObjectsController::class, 'add'])
+            ->setVisible(true)
+            ->setRouteAddition('/{class?}');
+        $this->addAction('ExecAdd')
+            ->setMethod('post')
+            ->addControllerAction([\Sunhill\Visual\Controllers\Database\ObjectsController::class, 'execadd'])
+            ->setVisible(false)
+            ->setRouteAddition('/{class}');
+        $this->addAction('Edit')
+            ->addControllerAction([\Sunhill\Visual\Controllers\Database\ObjectsController::class, 'edit'])
+            ->setVisible(false)
+            ->setRouteAddition('/{id}');
+        $this->addAction('ExecEdit')
+            ->addControllerAction([\Sunhill\Visual\Controllers\Database\ObjectsController::class, 'execedit'])
+            ->setVisible(false)
+            ->setRouteAddition('/{id}');
+        $this->addAction('Delete')
+            ->addControllerAction([\Sunhill\Visual\Controllers\Database\ObjectsController::class, 'delete'])
+            ->setVisible(false)
+            ->setRouteAddition('/{id}');
     }
     
     
