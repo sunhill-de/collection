6f57eb8975bbee87cfd5ed8fe0a8316ac4aca109
Klaus Dimde <klaus@dimde.info>
2023-01-29 20:32:46 +0100

    Added home

---
+++ Marketeers/Home/OpenHabItem.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Sunhill\InfoMarket\Marketeers\Home;
+
+use Sunhill\InfoMarket\Market\ObjectLeaf;
+
+class OpenHabItem extends ArrayLeaf
+{
+    protected function getCount(string $filter): int
+    {
+        $this->fillCache();
+        return count($this->cache->host);
+    }
+    
+    protected function getIndexValue(int $index, array $remains, string $order, string $filter)
+    {
+        $this->fillCache();
+        return new MacPingEntryItem($this->cache->host[$index]);
+    }
+    
+}
