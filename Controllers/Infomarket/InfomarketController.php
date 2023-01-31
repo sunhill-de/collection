ab51f8864c58d7de0f6a29e09f895ed735bac6ba
Klaus Dimde <klaus@dimde.info>
2023-01-31 16:57:29 +0100

    (re)implemented infomarket view

--- Controllers/Infomarket/InfomarketController.php
+++ Controllers/Infomarket/InfomarketController.php
@@ -3,7 +3,7 @@
 namespace Sunhill\Visual\Controllers\Infomarket;
 
 use Illuminate\Routing\Controller;
-use Sunhill\Visual\Response\SunhillTileViewResponse;
+use Sunhill\Visual\Response\InfoMarket\IndexResponse;
 use Sunhill\Visual\Response\Database\Classes\ListClassesResponse;
 use Sunhill\Visual\Response\Database\Classes\ShowClassResponse;
 
@@ -12,7 +12,7 @@ class InfomarketController extends Controller
     
     public function index()
     {
-        $response = new SunhillTileViewResponse();
+        $response = new IndexResponse();
         return $response->response();
     }
     
--- Response/InfoMarket/IndexResponse.php
+++ Response/InfoMarket/IndexResponse.php
@@ -2,18 +2,17 @@
 
 namespace Sunhill\Visual\Response\InfoMarket;
 
-use Sunhill\Visual\Response\BladeResponse;
+use Sunhill\Visual\Response\SunhillBladeResponse;
 use Sunhill\InfoMarket\Facades\InfoMarket;
 
-class IndexResponse extends BladeResponse
+class IndexResponse extends SunhillBladeResponse
 {
     
     protected $template = 'visual::infomarket.index';
  
     protected function prepareResponse()
     {
+       parent::prepareResponse();
        $this->params['entries'] = InfoMarket::getOffer(false); 
-       
-        
     }
 }
