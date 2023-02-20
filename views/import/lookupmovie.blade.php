cde02c220218e1e6ddcaefc8cb9e0e57b7f4afc0
klaus dimde <klaus@dimde.info>
2023-02-20 10:55:53 +0100

    Prepared lookup moview

---
+++ views/import/lookupmovie.blade.php
@@ -0,0 +1,11 @@
+@extends('visual::basic.navigation')
+
+@section('title',__('Lookup movie'))
+
+@section('caption')
+{{ __('Lookup movie') }}
+@endsection
+  
+@section('content')
+Lookup
+@endsection 
