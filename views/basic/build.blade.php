5bd008f47b3ee40c2ca0da5157430a22e1e0f8c5
Klaus Dimde <klaus@dimde.info>
2023-02-24 10:33:41 +0100

    Prepared for collection

--- views/basic/build.blade.php
+++
@@ -1,3 +0,0 @@
-@foreach ($files as $file)
-{!! $file !!}
-@endforeach
--- views/basic/tile.blade.php
+++
@@ -1,6 +0,0 @@
-<article class="tile is-child box">
- <p class="title">@yield('tilecaption')</p>
-  <div class="content">
-   @yield('tilebody')
-  </div>
-</article>
