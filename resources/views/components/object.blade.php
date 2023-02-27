b7eeaea54f818e039f60e34a94d84a21f397df99
Klaus Dimde <klaus@dimde.info>
2023-02-27 10:52:02 +0100

    Fixed wrong closing div tag bug

--- resources/views/components/object.blade.php
+++ resources/views/components/object.blade.php
@@ -5,8 +5,8 @@
   <input  class="input is-small" name="input_{{ $name }}" id="input_{{ $name }}"  @isset($key) value="{{ $obj_key }}" @endisset />
  </div>
  <input type="hidden" name="value_{{ $name }}" id="value_{{ $name }}" @isset($id) value="{{ $obj_id }}" @endisset/>
- </div>
- <script>
- 	$( function() { objectField('{{ $name }}', '{{ $class}}'); } );
- </script>
 </div>
+<script>
+ 	$( function() { objectField('{{ $name }}', '{{ $class}}'); } );
+</script>
+
