d8648c375205891fde9c178d6ea1e311d3153758
klaus dimde <klaus@dimde.info>
2023-03-04 22:13:01 +0100

    Add dummy test

---
+++ tests/Unit/Managers/ImportManagerTest.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Sunhill\Collection\Tests\Unit\Managers;
+
+use Sunhill\Collection\Importer\ImportManager;
+use Sunhill\InfoMarket\Tests\InfoMarketTest;
+
+class ImportManagerTest extends InfoMarketTest
+{
+
+}
