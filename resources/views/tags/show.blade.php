452d13208fa7a0356fd1a223cb4768da2d60c274
Klaus Dimde <klaus@dimde.info>
2023-03-07 13:35:33 +0100

    Fixed show tag

--- resources/views/tags/show.blade.php
+++ resources/views/tags/show.blade.php
@@ -9,5 +9,27 @@
 @section('content')
 @parent
 
+<table class="table">
+ <tr>
+  <th>{{ __('Field') }}</th>
+  <th>{{ __('Value') }}</th>
+ </tr>
+ <tr>
+  <td>{{ __('Name') }}</td>
+  <td>{{ $name }}</td>
+ </tr>
+ <tr>
+  <td>{{ __('Parent') }}</td>
+  <td>{{ $parent }}</td>
+ </tr>
+ <tr>
+  <td>{{ __('Full path') }}</td>
+  <td>{{ $fullpath }}</td>
+ </tr>
+  <tr>
+  <td>{{ __('Leafable') }}</td>
+  <td>{{ $leafable }}</td>
+ </tr> 
+</table> 
 @endsection
   
--- src/Response/Database/Tags/ShowTagResponse.php
+++ src/Response/Database/Tags/ShowTagResponse.php
@@ -3,6 +3,7 @@
 namespace Sunhill\Collection\Response\Database\Tags;
 
 use Sunhill\ORM\Facades\Tags;
+use Sunhill\ORM\Objects\Tag;
 use Sunhill\Visual\Response\SunhillBladeResponse;
 
 class ShowTagResponse extends SunhillBladeResponse
@@ -19,6 +20,11 @@ class ShowTagResponse extends SunhillBladeResponse
     protected function prepareResponse()
     {
         parent::prepareResponse();
+        $tag = Tags::loadTag($this->id);
+        $this->params['name'] = $tag->name;
+        $this->params['parent'] = ($tag->parent)?$tag->parent->name:'';
+        $this->params['fullpath'] = $tag->getFullpath();
+        $this->params['leafable'] = ($tag->options & Tag::TO_LEAFABLE)?__('yes'):__('no');
     }
     
 }  
