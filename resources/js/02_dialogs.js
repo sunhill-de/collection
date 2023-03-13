661198bf2cfeb04f289dc1fd4405cb01041d5526
Klaus Dimde <klaus@dimde.info>
2023-03-13 09:57:22 +0100

    Implemented attribute edit dialog elements

--- resources/js/02_dialogs.js
+++ resources/js/02_dialogs.js
@@ -105,3 +105,23 @@ function removeElement( caller ) {
 	caller.parent().remove();
 }
  
+function addAttribute() {
+	var attribute_name = $("#avaiable").val();
+	$.get("{{ asset("/ajax/") }}/getAttributeType/"+attribute_name , function(data) {
+		var append = '<div class="control"><label class="label is-size-7">'+attribute_name+'</label>';
+		switch (data) {
+			case "int":
+				append += '<input name="'+attribute_name+'" type="number" />'; break;
+		    case "char":
+				append += '<input name="'+attribute_name+'" type="text" />'; break;
+			case "float":			
+				append += '<input name="'+attribute_name+'" type="number" />'; break;
+			case "text":
+				append += '<textarea name="'+attribute_name+'"></textarea>'; break;
+			default: 
+				alert( data );	
+		}
+		append += "</div>";
+		$("#current_attributes").append(append);
+	})
+} 
