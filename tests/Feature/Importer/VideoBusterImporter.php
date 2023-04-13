8c6035bb6e34c5b22c17138c0ff4ba9ed3db1573
Klaus Dimde <klaus@dimde.info>
2023-04-13 14:04:37 +0200

    Prepared importer feature tests

---
+++ tests/Feature/Importer/VideoBusterImporter.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Sunhill\Collection\Tests\Feature\Importer;
+
+use Sunhill\Collection\Importer\VideoBusterImporter;
+use Sunhill\Collection\Tests\DatabaseTestCase;
+
+class VideoBusterImporterTest extends DatabaseTestCase
+{
+
+    
+}
