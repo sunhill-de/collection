8a2a664a3e1964237ac02daf61d0d44d52280a38
Klaus Dimde <klaus@dimde.info>
2023-04-14 13:46:57 +0200

    Implemented import file

--- src/Managers/ImportManager.php
+++ src/Managers/ImportManager.php
@@ -2,6 +2,8 @@
 
 namespace Sunhill\Collection\Managers;
 
+use Sunhill\Visual\Response\SunhillUserException;
+
 class ImportManager
 {
     
@@ -15,5 +17,41 @@ class ImportManager
     public function getFilters()
     {
         return $this->filters;
+    }
+    
+    protected function autodetectFile(string $name)
+    {
+        $content = file_get_content($name);
+        foreach ($this->filters as $name => $filter) {
+            if ($filter::autodetect($content)) {
+                return $filter;
+            }
+        }
+        return false;
+    }
+    
+    protected function callFilter(string $name, string $filter_class)
+    {
+        $filter_object = new $filter_class();
+        $filter_object->setImportFile($name);
+        return $filter_object->run();        
+    }
+    
+    public function importFile(string $name, string $filter = 'autodetect')
+    {
+        if (!file_exists($name)) {
+            throw new SunhillUserException(__("The file ':name' does not exist.",['name'=>$name]));
+        }
+        if ($filter == 'autodetect') {
+            if (!$filter_class = $this->autodetectFile($name)) {
+                throw new SunhillUserException(__("The file ':name' was not autodetectable.",['name'=>$name]));
+            }
+        } else {
+            if (!isset($this->filters[$filter])) {
+                throw new SunhillUserException(__("Unknown filter: ':name'.",['name'=>$filter]));                
+            }
+            $filter_class = $this->filters[$filter];
+        }
+        return $this->callFilter($name, $filter_class);
     }
 }
