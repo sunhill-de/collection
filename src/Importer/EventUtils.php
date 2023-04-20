846a97987493d665c53a076c697abd1fd3ddaf08
Klaus Dimde <klaus@dimde.info>
2023-04-20 11:43:05 +0200

    Add user

--- src/Importer/EventUtils.php
+++ src/Importer/EventUtils.php
@@ -8,9 +8,9 @@ use Illuminate\Support\Facades\DB;
 trait EventUtils
 {
     
-    protected function searchEvent(string $refering_table, int $refering_id, string $event_type, string $date)
+    protected function searchEvent(string $refering_table, int $refering_id, string $event_type, string $date, string $user)
     {
-        if ($result = DB::table('import_events')->where('refering_table',$refering_table)->where('refering_id',$refering_id)->where('event_type',$event_type)->where('date',$date)->first())
+        if ($result = DB::table('import_events')->where('refering_table',$refering_table)->where('refering_id',$refering_id)->where('event_type',$event_type)->where('date',$date)->where('user',$user)->first())
         {
             return $result->id;
         }
@@ -27,18 +27,18 @@ trait EventUtils
         return Carbon::createFromFormat('d.m.Y', trim($date))->format('Y-m-d');
     }
     
-    protected function insertEvent(string $refering_table, int $refering_id, string $event_type, string $date)
+    protected function insertEvent(string $refering_table, int $refering_id, string $event_type, string $date, string $user)
     {
-        DB::table('import_events')->insert(['refering_table'=>$refering_table,'refering_id'=>$refering_id,'event_type'=>$event_type,'date'=>$date]);
+        DB::table('import_events')->insert(['refering_table'=>$refering_table,'refering_id'=>$refering_id,'event_type'=>$event_type,'date'=>$date, 'user'=>$user]);
         return DB::getPdo()->lastInsertId();
     }
     
-    protected function searchOrInsertEvent(string $refering_table, int $refering_id, string $event_type, string $date)
+    protected function searchOrInsertEvent(string $refering_table, int $refering_id, string $event_type, string $date, string $user)
     {
-        if ($result = $this->searchEvent($refering_table,$refering_id,$event_type,$date)) {
+        if ($result = $this->searchEvent($refering_table,$refering_id,$event_type,$date, $user)) {
             return $result;
         }
-        return $this->insertEvent($refering_table,$refering_id,$event_type,$date);
+        return $this->insertEvent($refering_table,$refering_id,$event_type,$date, $user);
     }
     
     protected function eventIsAlreadyImported(int $id)
