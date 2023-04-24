aeb4b9bbffbab0fbb67b157a4104ab85432a9275
Klaus Dimde <klaus@dimde.info>
2023-04-24 09:49:24 +0200

    adjusted to tmdb

--- resources/views/import/movies/lookup.blade.php
+++ resources/views/import/movies/lookup.blade.php
@@ -10,34 +10,40 @@
 <form action="{{ route('imports.movies.execlookup',['id'=>$id]) }}" method="post">
   @csrf
  <div class="control">
- @foreach($results as $result)
- <input type="radio" name="imdb" id="imdb" value="{{$result['id']}}" @if($loop->first)checked @endif>
+ @foreach($results as $id => $result)
+ <input type="hidden" name="imdb" id="imdb" value="{{$result->imdb_id}}"/>
+ <input type="radio" name="tmdb" id="tmdb" value="{{$result->id}}" @if($loop->first)checked @endif>
+ 
 <div class="card">
   <div class="card-content">
     <div class="media">
       <div class="media-left">
         <figure class="image is-48x48">
-          <img src="{{ $result['image'] }}" alt="Placeholder image">
+          @if(isset($result->posters(300)[0]))
+          <img src="{{ $result->posters(300)[0]->file_path }}" alt="Placeholder image">
+          @endif
         </figure>
       </div>
       <div class="media-content">
-        <p class="title is-4">{{ $result['title'] }} ({{ $result['year'] }})</p>
-        <p class="subtitle is-6">{{ __('length: :length', ['length'=>$result['length']]) }}<br>
-        @foreach ($result['genres'] as $genre)
+        <p class="title is-4">{{ $result->title }} ({{ explode('-',$result->release_date)[0] }})</p>
+        <p class="subtitle is-6">{{ __('length: :length', ['length'=>$result->runtime]) }}<br>
+        @foreach ($result->genres as $genre)
          @if (!$loop->first)*@endif
-         {{$genre}}
+         {{$genre->name}}
         @endforeach        
         </p>
       </div>
     </div>
 
     <div class="content">
-      {{ $result['plot'] }}<br>
+      {{ $result->overview }}<br>
       {{ __('Cast') }}:
-      @foreach($result['cast'] as $cast)
+      @if(isset($result->casts()['cast']))
+      @foreach(array_slice($result->casts()['cast'],0,5) as $cast)
        @if (!$loop->first) * @endif
-       <b>{{ $cast['actor'] }}</b> ({{$cast['character']}})
+       <b>{{ $cast->name }}</b> ({{$cast->character}})
       @endforeach
+      @endif
     </div>
   </div>
  </div> 
