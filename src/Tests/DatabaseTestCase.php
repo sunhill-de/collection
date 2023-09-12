6e1423b8c19162b79e705b847b3c71f73296148a
Klaus Dimde <klaus@dimde.info>
2023-09-12 23:36:43 +0200

    Fixed seeding

--- src/Tests/DatabaseTestCase.php
+++ src/Tests/DatabaseTestCase.php
@@ -11,7 +11,6 @@ use Sunhill\Collection\Modules\Database\SunhillFeatureTags;
 use Sunhill\Collection\Modules\Database\SunhillFeatureAttributes;
 use Sunhill\Collection\Modules\Database\SunhillFeatureImports;
 
-use Sunhill\Collection\Objects\FamilyMember;
 use Sunhill\Collection\Collections\Language;
 use Sunhill\ORM\Facades\Collections;
 
@@ -24,6 +23,12 @@ use Sunhill\Collection\Objects\Creative\TVSeries;
 use Sunhill\Collection\Objects\Locations\City;
 use Sunhill\Collection\Objects\Locations\Street;
 use Sunhill\Collection\Objects\Locations\Address;
+use Sunhill\Collection\Collections\ProductGroup;
+
+use Sunhill\Collection\Objects\Persons\Person;
+use Sunhill\Collection\Objects\Persons\FamilyMember;
+
+use Sunhill\Collection\Collections\PersonsRelation;
 
 class DatabaseTestCase extends CollectionTestCase
 {
@@ -66,31 +71,73 @@ class DatabaseTestCase extends CollectionTestCase
         });
             SunhillSiteManager::installRoutes();
     }
-    
-    protected function seedDatabase()
+
+    protected function seedProductionGroups()
     {
-        $germany = Country::seed([['name'=>'germany','iso_code'=>'D']]);
-        $france  = Country::seed([['name'=>'france','iso_code'=>'F']]);
-        $spain = Country::seed([['name'=>'spain','iso_code'=>'E']]);
-        $usa = Country::seed([['name'=>'U.S.A.','iso_code'=>'USA']]);
-        $gb = Country::seed([['name'=>'Great Britain','iso_code'=>'GB']]);
-        
-        $springfield = City::seed([['name'=>'Springfield','part_of'=>$usa]]);
-        $berlin = City::seed([['name'=>'Berlin','part_of'=>$germany]]);
-        $madrid = City::seed([['name'=>'Madrid','pard_of'=>$spain]]);
-        
-        $evergreen = Street::seed([['name'=>'Evergreen Terrace','part_of'=>$springfield]]);
-        $kudamm = Street::seed([['name'=>'Kufürstendamm', 'part_of'=>$berlin]]);
-        
-        $simpsons = Address::seed([['house_number'=>'742']]);
-        
-        $english = Language::seed([['name'=>'english','iso'=>'en','translations'=>['en'=>'english','de'=>'englisch']]]);
-        $german  = Language::seed([['name'=>'german','iso'=>'de','translations'=>['en'=>'german','de'=>'deutsch']]]);
-        $french  = Language::seed([['name'=>'french','iso'=>'fr','translations'=>['en'=>'french','de'=>'französich']]]);
-        $spanish = Language::seed([['name'=>'spanish','iso'=>'es','translations'=>['en'=>'spanish','de'=>'spanisch']]]);
-        
-        $lost = TVSeries::seed(
-            [
+        ProductGroup::seed(['food'=>['name'=>'food']]);
+        ProductGroup::seed(['nonfood'=>['name'=>'nonfood']]);
+        ProductGroup::seed(['electronics'=>['name'=>'electronics','part_of'=>ProductGroup::getSeedID('nonfood')]]);
+        ProductGroup::seed(['computer'=>['name'=>'computer', 'part_of'=>ProductGroup::getSeedID('electronics')]]);
+        ProductGroup::seed(['beer'=>['name'=>'beer','part_of'=>ProductGroup::getSeedID('food')]]);        
+    }
+    
+    protected function seedPersons()
+    {
+        Person::seed([
+            'norton'=>['firstname'=>'Edward','lastname'=>'Norton','sex'=>'male','groups'=>['actor']],
+            'pitt'=>['firstname'=>'Brad','lastname'=>'Pitt','sex'=>'male','groups'=>['actor']],
+            'carter'=>['firstname'=>'Helena','lastname'=>'Bonham Carter','sex'=>'female','groups'=>['actor']],
+            'fincher'=>['firstname'=>'David','lastname'=>'Fincher','sex'=>'male','groups'=>['director']]
+        ]);            
+    }
+    
+    protected function seedCountries()
+    {
+        Country::seed([
+            'germany'=>['name'=>'germany','iso_code'=>'D'],
+            'france'=>['name'=>'france','iso_code'=>'F'],
+            'spain'=>['name'=>'spain','iso_code'=>'E'],
+            'usa'=>['name'=>'U.S.A.','iso_code'=>'USA'],
+            'gb'=>['name'=>'Great Britain','iso_code'=>'GB'],
+         ]);   
+    }
+    
+    protected function seedCities()
+    {
+        City::seed([
+            'springfield'=>['name'=>'Springfield','part_of'=>City::getSeedID('usa')],
+            'berlin'=>['name'=>'Berlin','part_of'=>City::getSeedID('germany')],
+            'madrid'=>['name'=>'Madrid','part_of'=>City::getSeedID('spain')],
+        ]);    
+    }
+    
+    protected function seedStreets()
+    {
+        Street::seed([
+            'evergreen'=>['name'=>'Evergreen Terrace','part_of'=>Street::getSeedID('springfield')],
+            'kudamm'=>['name'=>'Kufürstendamm', 'part_of'=>Street::getSeedID('berlin')],
+        ]);    
+    }
+    
+    protected function seedAddresses()
+    {
+        Address::seed(['simpsons'=>['name'=>'742 Evergreen Terrace','house_number'=>'742','part_of'=>Street::getSeedID('evergreen')]]);        
+    }
+
+    protected function seedLanguages()
+    {
+        Language::seed([
+            'english'=>['name'=>'english','iso'=>'en','translations'=>['en'=>'english','de'=>'englisch']],
+            'german'=>['name'=>'german','iso'=>'de','translations'=>['en'=>'german','de'=>'deutsch']],
+            'french'=>['name'=>'french','iso'=>'fr','translations'=>['en'=>'french','de'=>'französich']],
+            'spanish'=>['name'=>'spanish','iso'=>'es','translations'=>['en'=>'spanish','de'=>'spanisch']],
+        ]);        
+    }
+    
+    protected function seedTVSeries()
+    {
+        TVSeries::seed(
+          ['lost'=>[
                 'name'=>'Lost',
                 'original_name'=>'Lost',
                 'sort_name'=>'LOST',
@@ -99,7 +146,13 @@ class DatabaseTestCase extends CollectionTestCase
                 'tmdb_id'=>'/tv/4609-lost',
                 'number_of_episodes'=>121,
                 'number_of_seasons'=>6
-            ]);
+           ],               
+          ]);
+        
+    }
+    
+    protected function seedMovies()
+    {
         Movie::seed(
             [
                 [
@@ -107,20 +160,20 @@ class DatabaseTestCase extends CollectionTestCase
                     'original_name'=>'Fight Club',
                     'sort_name'=>'FIGHTCLUB',
                     'release_date'=>'1999-11-11',
-                    'language'=>$english,
-                    'countries'=>[$usa],
+                    'language'=>Movie::getSeedID('english'),
+                    'countries'=>[Movie::getSeedID('usa')],
                     'length'=>139,
                     'imdb_id'=>'tt0137523',
                     'tmdb_id'=>'/movie/550-fight-club',
                     'keywords'=>['fight','anarchy']
-                ],    
+                ],
                 [
                     'name'=>'Die Stadt der verlorenen Kinder',
                     'original_name'=>'La Cité des Enfants Perdus',
                     'sort_name'=>'STADTDERVERLORENENKINDER, DIE',
                     'release_date'=>'1995-08-17',
-                    'language'=>$french,
-                    'countries'=>[$france],    
+                    'language'=>Movie::getSeedID('french'),
+                    'countries'=>[Movie::getSeedID('france')],
                     'length'=>112,
                     'imdb_id'=>'tt0112682',
                     'tmdb_id'=>'/movie/902-la-cite-des-enfants-perdus',
@@ -128,62 +181,130 @@ class DatabaseTestCase extends CollectionTestCase
                 ],
             ]);
         
-        $watch_event  = EventType::seed([['name'=>'watch','translations'=>['en'=>'watch','de'=>'sehen']]]);
-        $change_event = EventType::seed([['name'=>'change','translations'=>['en'=>'change','de'=>'ändern']]]);
-        $switch_event = EventType::seed([['name'=>'switch','translations'=>['en'=>'switch','de'=>'umschalten']]]);
-        
-        $fiction = Genre::seed([
-            [
+    }
+    
+    protected function seedEventTypes()
+    {
+        EventType::seed([
+            'watch'=>['name'=>'watch','translations'=>['en'=>'watch','de'=>'sehen']],
+            'change'=>['name'=>'change','translations'=>['en'=>'change','de'=>'ändern']],
+            'switch'=>['name'=>'switch','translations'=>['en'=>'switch','de'=>'umschalten']]            
+        ]);        
+    }
+    
+    protected function seedGenres()
+    {
+        Genre::seed([
+            'fiction'=>[
                 'name'=>'fiction',
                 'media_type'=>['VisualWork','WrittenWork'],
                 'translations'=>['en'=>'fiction','de'=>'Fiktion']
-            ]
-        ]);
-        $nonfiction = Genre::seed([
-            [
+            ],
+            'nonfiction'=>[
                 'name'=>'nonfiction',
                 'media_type'=>['VisualWork','WrittenWork'],
                 'translations'=>['en'=>'nonfiction','de'=>'Nichtfiktion']
             ]
         ]);
+
         Genre::seed([
             [
                 'name'=>'science fiction',
                 'media_type'=>['VisualWork','WrittenWork'],
                 'translations'=>['en'=>'science fiction','de'=>'Science fiction'],
-                'parent'=>$fiction
+                'parent'=>Movie::getSeedID('fiction')
             ],
             [
                 'name'=>'drama',
                 'media_type'=>['VisualWork','WrittenWork'],
                 'translations'=>['en'=>'drama','de'=>'Drama'],
-                'parent'=>$fiction
-            ],            
+                'parent'=>Movie::getSeedID('fiction')
+            ],
             [
                 'name'=>'horror',
                 'media_type'=>['VisualWork','WrittenWork'],
                 'translations'=>['en'=>'horror','de'=>'Horror'],
-                'parent'=>$fiction
+                'parent'=>Movie::getSeedID('fiction')
             ],
             [
                 'name'=>'computer',
                 'media_type'=>['WrittenWork'],
                 'translations'=>['en'=>'computer','de'=>'Computer'],
-                'parent'=>$nonfiction
+                'parent'=>Movie::getSeedID('nonfiction')
             ],
             [
                 'name'=>'programming',
                 'media_type'=>['VisualWork','WrittenWork'],
                 'translations'=>['en'=>'programming','de'=>'Programmierung'],
-                'parent'=>$nonfiction
+                'parent'=>Movie::getSeedID('nonfiction')
             ],
         ]);
         
-        $homer = FamilyMember::seed([['firstname'=>'Homer','middlename'=>'Jay','lastname'=>'Simpson','sex'=>'male','date_of_birth'=>"1956-05-12",'address'=>$simpsons]]);
-        $marge = FamilyMember::seed([['firstname'=>'Marge','lastname'=>'Simpson','sex'=>'female',"birth_name"=>"Bouvier",'address'=>$simpsons]]);
-        $bart = FamilyMember::seed([['firstname'=>"Bart","lastname"=>"Simpson","sex"=>"male","date_of_birth"=>"1980-02-23",'father'=>$homer,'mother'=>$marge,'address'=>$simpsons]]);
-        $lisa = FamilyMember::seed([['firstname'=>"Lisa","lastname"=>"Simpson","sex"=>"female","date_of_birth"=>"1981-05-09",'father'=>$homer,'mother'=>$marge,'address'=>$simpsons]]);
-        $maggie = FamilyMember::seed([['firstname'=>"Maggie","lastname"=>"Simpson","sex"=>"female",'father'=>$homer,'mother'=>$marge,'address'=>$simpsons]]);
+    }
+    
+    protected function seedFamilyMembers()
+    {
+        $homer = FamilyMember::seed([
+            'homer'=>['firstname'=>'Homer','middlename'=>'Jay','lastname'=>'Simpson','sex'=>'male','date_of_birth'=>"1956-05-12",'address'=>FamilyMember::getSeedID('simpsons')],            
+            'marge'=>['firstname'=>'Marge','lastname'=>'Simpson','sex'=>'female',"birth_name"=>"Bouvier",'address'=>FamilyMember::getSeedID('simpsons')]            
+        ]);
+        FamilyMember::seed([
+            [
+                'firstname'=>"Bart",
+                "lastname"=>"Simpson",
+                "sex"=>"male",
+                "date_of_birth"=>"1980-02-23",
+                'father'=>FamilyMember::getSeedID('homer'),
+                'mother'=>FamilyMember::getSeedID('marge'),
+                'address'=>FamilyMember::getSeedID('simpsons')                
+            ],
+            [
+                'firstname'=>"Lisa",
+                "lastname"=>"Simpson",
+                "sex"=>"female",
+                "date_of_birth"=>"1981-05-09",
+                'father'=>FamilyMember::getSeedID('homer'),
+                'mother'=>FamilyMember::getSeedID('marge'),
+                'address'=>FamilyMember::getSeedID('simpsons')                
+            ],
+            [
+                'firstname'=>"Maggie",
+                "lastname"=>"Simpson",
+                "sex"=>"female",
+                'father'=>FamilyMember::getSeedID('homer'),
+                'mother'=>FamilyMember::getSeedID('marge'),
+                'address'=>FamilyMember::getSeedID('simpsons')                
+            ]            
+        ]);
+        
+    }
+    
+    protected function seedPersonsRelations()
+    {
+        PersonsRelation::seed([
+            [
+                'person1'=>FamilyMember::getSeedID('homer'),
+                'person2'=>FamilyMember::getSeedID('homer'),
+                'relation'=>'marriage'                
+            ]
+        ]);        
+    }
+    
+    protected function seedDatabase()
+    {
+        $this->seedProductionGroups();
+        $this->seedPersons();
+        $this->seedCountries();
+        $this->seedCities();
+        $this->seedStreets();
+        $this->seedAddresses();        
+        $this->seedLanguages();
+        $this->seedTVSeries();
+        $this->seedMovies();
+        $this->seedEventTypes();
+        $this->seedGenres();
+        $this->seedFamilyMembers();
+        $this->seedPersonsRelations();
     }
     
 }
