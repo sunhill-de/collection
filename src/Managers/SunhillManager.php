01627f5b331966269add140607fb7b9b74577e9b
Klaus Dimde <klaus@dimde.info>
2023-09-13 12:31:06 +0200

    Utilities for listing collections

--- src/Managers/SunhillManager.php
+++ src/Managers/SunhillManager.php
@@ -17,10 +17,12 @@ use Sunhill\Collection\Collections\StaffJob;
 use Sunhill\ORM\Facades\Collections;
 use Sunhill\Collection\Collections\Language;
 use Sunhill\Collection\Collections\EventType;
+use Sunhill\ORM\Objects\PropertiesCollection;
 
 class SunhillManager
 {
-    
+   
+ // ****************************** Collection helpers *************************************   
     /**
      * Searches for a staff job with the given name. If none found return null
      * @param string $job
@@ -112,4 +114,83 @@ class SunhillManager
         return $result;
     }
     
+// ******************************** List helpers **********************************************
+    protected function getTableColumn(PropertiesCollection $collection, string $column)
+    {
+        if (strpos($column,'->') === false) {
+            $result = $collection->$column;
+            return $result;
+        } else {
+            list($field,$subfield) = explode('->',$column);
+            $field = $collection->$field;
+            if (!empty($field)) {
+                return $field->$subfield;
+            }
+            return '';
+        }
+    }
+    
+    /**
+     * Returns the defines keyfield or just uuid if no keyfield was set
+     * @param PropertiesCollection $collection
+     * @return unknown
+     */
+    public function getKeyfield(PropertiesCollection $collection)
+    {
+        $keyfield = $collection::getInfo('keyfield', ':_uuid');
+        
+        $vars = preg_match_all('/\:(\S+)/s',$keyfield,$matches);
+        foreach ($matches[1] as $match) {
+            $keyfield = str_replace(':'.$match,$this->getTableColumn($collection,$match),$keyfield);
+        }
+        return $keyfield;        
+    }
+    
+    protected function getTableRow(PropertiesCollection $collection)
+    {
+        $result = [];
+        
+        foreach ($collection::getInfo('table_columns',['uuid'=>'_uuid']) as $name => $column) {
+            if (is_numeric($name)) {
+                $result[$column] = $this->getTableColumn($collection, $column);                
+            } else {
+                $result[$name] = $this->getTableColumn($collection, $column);
+            }
+        }
+        
+        return $result;
+    }
+
+    /**
+     * Returns the entries of a list of the given collection with the given conditions, orderings and limitations
+     * @param string $collection_name
+     * @param array $conditions
+     * @param string $order
+     * @param string $order_direction
+     * @param int $offset
+     * @param int $limit
+     * @return array of array of string
+     */
+    public function getCollectionList(string $collection_name, array $conditions = [], string $order = 'id', string $order_direction = 'asc', int $offset = 0, int $limit = 10)
+    {
+        $namespace = Collections::searchCollection($collection_name);
+        $query = $namespace::search();
+        foreach ($conditions as $condition) {
+            $query->where($condition['key'],$condition['relation'],$condition['value']);
+        }
+        $query->orderBy($order, $order_direction);
+        if ($offset) {
+            $query->offset($offset);
+        }
+        $query->limit($limit);
+        
+        $entries = $query->get();
+        
+        $result = [];
+        foreach ($entries as $entry) {
+            $collection = Collections::loadCollection($namespace, $entry->id);
+            $result[] = $this->getTableRow($collection);
+        }
+        return $result;
+    }
 }
