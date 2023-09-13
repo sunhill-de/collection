45d36da9751340d036f41f848cc4ff151a4d2fd4
Klaus Dimde <klaus@dimde.info>
2023-09-13 10:00:44 +0200

    Helper routines for staffjobs, eventtypes and languages

--- src/Managers/SunhillManager.php
+++ src/Managers/SunhillManager.php
@@ -15,6 +15,8 @@ namespace Sunhill\Collection\Managers;
 
 use Sunhill\Collection\Collections\StaffJob;
 use Sunhill\ORM\Facades\Collections;
+use Sunhill\Collection\Collections\Language;
+use Sunhill\Collection\Collections\EventType;
 
 class SunhillManager
 {
@@ -22,7 +24,69 @@ class SunhillManager
     /**
      * Searches for a staff job with the given name. If none found return null
      * @param string $job
+     * @return null|EventType
+     * Test: SunhillManagerTest::testSearchEventType
+     */
+    public function searchEventType(string $name)
+    {
+        if ($result =  EventType::search()->where('name',$name)->first()) {
+            return Collections::loadCollection('EventType', $result->id);
+        }
+    }
+    
+    /**
+     * Searches for a event type with the given name. if none found create one and return it
+     * @param string $job
+     * @return EventType
+     * Test: SunhillManagerTest::testSearchOrInsertEventType
+     */
+    public function searchOrInsertEventType(string $name): EventType
+    {
+        if ($result = $this->searchEventType($name)) {
+            return $result;
+        }
+        $result = new EventType();
+        $result->name = $name;
+        $result->commit();
+        return $result;
+    }
+    
+    /**
+     * Searches for a language with the given name. If none found return null
+     * @param string $language
+     * @return null|Language
+     * Test: SunhillManagerTest::testSearchLanguage
+     */
+    public function searchLanguage(string $language)
+    {
+        if ($result =  Language::search()->where('name',$language)->first()) {
+            return Collections::loadCollection('Language', $result->id);
+        }
+    }
+    
+    /**
+     * Searches for a language with the given name. if none found create one and return it
+     * @param string $job
+     * @return StaffJob
+     * Test: SunhillManagerTest::testSearchOrInsertLanguage
+     */
+    public function searchOrInsertLanguage(string $language, string $iso=''): Language
+    {
+        if ($result = $this->searchLanguage($language)) {
+            return $result;
+        }
+        $result = new Language();
+        $result->name = $language;
+        $result->iso = $iso;
+        $result->commit();
+        return $result;
+    }
+    
+    /**
+     * Searches for a staff job with the given name. If none found return null
+     * @param string $job
      * @return null|StaffJob
+     * Test: SunhillManagerTest::testSearchStaffJob
      */
     public function searchStaffJob(string $job)
     {
@@ -35,6 +99,7 @@ class SunhillManager
      * Searches for a staff job with the given name. if none found create one and return it
      * @param string $job
      * @return StaffJob
+     * Test: SunhillManagerTest::testSearchOrInsertStaffJob
      */
     public function searchOrInsertStaffJob(string $job): StaffJob
     {
@@ -43,6 +108,7 @@ class SunhillManager
         }
         $result = new StaffJob();
         $result->name = $job;
+        $result->commit();
         return $result;
     }
     
