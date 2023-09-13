deb420c427a45515721c01287e6d49ac62e00813
Klaus Dimde <klaus@dimde.info>
2023-09-13 10:01:27 +0200

    Tests for helper functions

--- tests/Unit/Managers/SunhillManagerTest.php
+++ tests/Unit/Managers/SunhillManagerTest.php
@@ -7,7 +7,34 @@ use Sunhill\Collection\Facades\SunhillManager;
 
 class SunhillManagerTest extends DatabaseTestCase
 {
-
+    public function testSearchEventType()
+    {
+        $this->assertEquals('watch',SunhillManager::searchEventType('watch')->name);
+        $this->assertNull(SunhillManager::searchEventType('notexisting'));
+    }
+    
+    public function testSearchOrInsertEventType()
+    {
+        $this->assertEquals('watch',SunhillManager::searchOrInsertEventType('watch')->name);
+        $this->assertEquals('listen',SunhillManager::searchOrInsertEventType('listen')->name);
+        $this->assertDatabaseHas('eventtypes',['name'=>'listen']);
+    }
+    
+    public function testSearchLanguage()
+    {
+        $this->assertEquals('english',SunhillManager::searchLanguage('english')->name);
+        $this->assertNull(SunhillManager::searchLanguage('notexisting'));
+    }
+    
+    public function testSearchOrInsertLanguage()
+    {
+        $this->assertEquals('english',SunhillManager::searchOrInsertLanguage('english')->name);
+        $this->assertEquals('polish',SunhillManager::searchOrInsertLanguage('polish','pl')->name);
+        $this->assertEquals('greek',SunhillManager::searchOrInsertLanguage('greek')->name);
+        $this->assertDatabaseHas('languages',['name'=>'polish']);
+        $this->assertDatabaseHas('languages',['name'=>'greek']);
+    }
+    
     public function testSearchStaffJob()
     {
         $this->assertEquals('actor',SunhillManager::searchStaffJob('actor')->name);
@@ -18,5 +45,7 @@ class SunhillManagerTest extends DatabaseTestCase
     {
         $this->assertEquals('actor',SunhillManager::searchOrInsertStaffJob('actor')->name);
         $this->assertEquals('producer',SunhillManager::searchOrInsertStaffJob('producer')->name);
+        $this->assertDatabaseHas('staffjobs',['name'=>'producer']);
     }
+    
 }
