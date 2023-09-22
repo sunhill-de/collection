71c013c142a38a7cf8f8732259412c58b4f30489
Klaus Dimde <klaus@dimde.info>
2023-09-22 09:51:53 +0200

    Fixed blade template

--- resources/views/collection/list.blade.php
+++ resources/views/collection/list.blade.php
@@ -1,27 +1,16 @@
-@extends('visual::basic.list')
+@extends('visual::basic.list')  
 
-@section('title',__('List objects'))
+@section('title',__('List collection objects'))
 
 @section('caption')
-{{ __("List objects of ':key'",['key'=>$key]) }} 
-@endsection
-
-@section('tableheader')
-<div class="hirarchy">
- <ul>
- <li>{{ __("Object hirarchy") }}</li>  
-  @foreach ($inheritance as $ancestor)
-  <li><a href="{{ route('objects.list',['key'=>$ancestor]) }}">{{ $ancestor }}</a></li>
-  @endforeach  
- </ul>
-</div>
+{{ __("List collection objects of ':collection'",['collection'=>$collectionname]) }} 
 @endsection
 
 @section('tablefooter')
-@if ($namespace::getInfo('instantiable', false))
+@if ($instantiable)
 <button>   
-<a href="{{ route('objects.add',['class'=> $key]) }}">{{ __('add') }}</a>
-</button>
+<a href="{{ route('collection.add',['collection'=> $collectionname]) }}">{{ __('add') }}</a>
+</button> 
 @endif
 @endsection
 
