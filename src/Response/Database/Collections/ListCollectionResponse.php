3d6dd0310e619fe4449f35feb32694411a4864d9
Klaus Dimde <klaus@dimde.info>
2023-09-22 09:55:28 +0200

    Implemented ListCollection

--- src/Response/Database/Collections/ListCollectionResponse.php
+++ src/Response/Database/Collections/ListCollectionResponse.php
@@ -10,12 +10,18 @@ use Sunhill\Visual\Facades\Dialogs;
 use Sunhill\Collection\Facades\SunhillManager;
 use Sunhill\ORM\Facades\Collections;
 
+/**
+ * In constract to the response in plural (ListCollectionsResponse) this response lists the objects of @author klaus
+ * certain collection.
+ * @author klaus
+ *
+ */
 class ListCollectionResponse extends SunhillListResponse
 {
     
-    protected $template = 'collection::collections.list';
+    protected $template = 'collection::collection.list';
     
-    protected $route = 'collections.list';
+    protected $route = 'collection.list';
     
     protected $collection = '';
     
@@ -34,11 +40,20 @@ class ListCollectionResponse extends SunhillListResponse
      */
     protected function defineList(ListDescriptor &$descriptor)
     {
-        $descriptor->column('name')->title('Name')->searchable();
-        $descriptor->column('description')->title('Description');
-        $descriptor->column('list')->link('collection.list',['collection'=>'name']);
-        $descriptor->column('add')->link('collection.add',['collection'=>'name']);
-        $descriptor->column('show')->link('collections.show',['collection'=>'name']);
+        $descriptor->column('id')->title('ID')->searchable();
+        $namespace = Collections::searchCollection($this->collection);
+        $columns = $namespace::getInfo('table_columns',[]);
+        foreach ($columns as $index => $column) {
+            if (is_int($index)) {
+                $column_desc = $descriptor->column($column)->title($column);
+            } else {
+                $column_desc = $descriptor->column($index)->title($index);
+            }
+        }
+        $descriptor->column('edit')->link('collection.edit',['collection'=>'class','id'=>'id']);
+        $descriptor->column('delete')->link('collection.delete',['collection'=>'class','id'=>'id']);
+        $descriptor->column('show')->link('collection.show',['collection'=>'class','id'=>'id']); 
+        
     }
     
     protected function handleConditions($query, $conditions)
@@ -55,9 +70,23 @@ class ListCollectionResponse extends SunhillListResponse
         return SunhillManager::getCollectionCount($this->collection, []);
     }
     
+    protected function getRouteParameters()
+    {
+        $result = parent::getRouteParameters();
+        $result['collection'] = $this->collection;
+        return $result;
+    }
+    
     protected function getData()
     {
         return SunhillManager::getCollectionList($this->collection, [], $this->order, $this->order_dir, $this->offset*SELF::ENTRIES_PER_PAGE,self::ENTRIES_PER_PAGE);
     }
 
+    public function prepareResponse()
+    {
+        parent::prepareResponse();
+        $infos = SunhillManager::getCollectionListParameters($this->collection);
+        $this->params = array_merge($this->params, $infos);
+    }
+    
 }  
