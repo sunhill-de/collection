dbf531c71ec901c540cac54f459759eec044886e
Klaus Dimde <klaus@dimde.info>
2023-09-27 21:59:48 +0200

    Some test for ajax

--- tests/Feature/html_tests/FeatureAjaxTest.php
+++ tests/Feature/html_tests/FeatureAjaxTest.php
@@ -8,8 +8,28 @@ class FeatureAjaxTest extends HtmlTestBase
     public static function HTMLProvider()
     {
         return [
-            ['/ajax/searchTags?search=Fa',200],       // Search Tags
+            ['/ajax/tags?search=Fa',200],       // Search Tags
         ];    
+    }
+    
+    public function testAjaxTags()
+    {
+        $response = $this->getJson('/ajax/tags?search=ar');
+        $response
+        ->assertStatus(200)
+        ->assertJson([['label'=>'Family.Marge','id'=>3],['label'=>'Family.Bart','id'=>4]]);
+    }
+    
+    public function testAjaxClasses()
+    {
+        $response = $this->getJson('/ajax/classes?search=Creative');
+        $response
+        ->assertStatus(200)
+        ->assertJson([
+            ['label'=>'CreativeCollection','id'=>'CreativeCollection'],
+            ['label'=>'CreativeStandaloneWork','id'=>'CreativeStandaloneWork'],
+            ['label'=>'CreativeWork','id'=>'CreativeWork'],            
+        ]);
     }
     
 }
