184ede29b978f7cc76ff04e9e2db7bd827e57d57
Klaus Dimde <klaus@dimde.info>
2023-09-29 13:19:04 +0200

    Fixed view

--- src/Response/Database/Tags/ListChildrenResponse.php
+++ src/Response/Database/Tags/ListChildrenResponse.php
@@ -4,11 +4,14 @@ namespace Sunhill\Collection\Response\Database\Tags;
 
 use Sunhill\ORM\Facades\Tags;
 use Sunhill\Visual\Response\Lists\ListDescriptor;
-use Sunhill\Visual\Response\SunhillListResponse;
+use Sunhill\Visual\Response\Lists\SunhillListResponse;
+use Sunhill\Collection\Utils\HasID;
 
 class ListChildrenResponse extends SunhillListResponse
 {
 
+    use HasID;
+    
     protected $template = 'collection::tags.list';
         
     protected $route = 'tags.listchildren';
@@ -17,13 +20,20 @@ class ListChildrenResponse extends SunhillListResponse
     
     protected function defineList(ListDescriptor &$descriptor)
     {
-        $descriptor->column('class')->title('Class name')->searchable();
+        $descriptor->column('id')->title('id')->searchable();
         $descriptor->column('name')->title('Name')->searchable();
-        $descriptor->column('description')->title('Description');
-        $descriptor->column('parent')->title('Parent');
-        $descriptor->column('list')->link('objects.list',['key'=>'name']);
-        $descriptor->column('add')->link('objects.add',['class'=>'name']);
-        $descriptor->column('show')->link('classes.show',['class'=>'name']);
+        $descriptor->column('parent')->title('Parent')->setCallback(function($data, $key) {
+            if ($data->parent_id) {
+                $tag = Tags::loadTag($data->parent_id);
+                return $tag->name;
+            } else {
+                return "";
+            }
+        });
+            $descriptor->column('fullpath')->title('Full path');
+            $descriptor->column('edit')->link('tags.edit',['id'=>'id'])->setLinkTitle('edit');
+            $descriptor->column('show')->link('tags.show',['id'=>'id'])->setLinkTitle('show');
+            $descriptor->column('delete')->link('tags.delete',['id'=>'id'])->setLinkTitle('delete');
     }
     
     /**
@@ -32,10 +42,12 @@ class ListChildrenResponse extends SunhillListResponse
      */
     protected function getEntryCount(): int
     {
+        return Tags::query()->where('parent_id', '=', $this->id)->count();
     }
     
     protected function getData()
     {
+        return Tags::query()->where('parent_id', '=', $this->id)->offset($this->offset*self::ENTRIES_PER_PAGE)->limit(self::ENTRIES_PER_PAGE)->get();
     }
     
   
