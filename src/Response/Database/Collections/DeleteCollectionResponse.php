46701d2406db9323a216fb80eb7f0c587e139bc2
Klaus Dimde <klaus@dimde.info>
2023-10-08 17:15:14 +0200

    Corrected return route

--- src/Response/Database/Collections/DeleteCollectionResponse.php
+++ src/Response/Database/Collections/DeleteCollectionResponse.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Sunhill\Collection\Response\Database\Objects;
+namespace Sunhill\Collection\Response\Database\Collections;
 
 use Sunhill\ORM\Facades\Classes;
 use Sunhill\ORM\Facades\Objects;
@@ -8,7 +8,7 @@ use Sunhill\Visual\Response\SunhillRedirectResponse;
 use Sunhill\Visual\Response\SunhillUserException;
 use Sunhill\Collection\Utils\HasID;
 
-class DeleteObjectResponse extends SunhillRedirectResponse
+class DeleteCollectionResponse extends SunhillRedirectResponse
 {
     
     use HasID;
@@ -21,7 +21,7 @@ class DeleteObjectResponse extends SunhillRedirectResponse
             throw new SunhillUserException(__("The object with the id ':id' does not exist.",['id'=>$this->id]));
         }
         $object->delete();
-        $this->setTarget(route('objects.list'));        
+        $this->setTarget(route('collection.list'));        
     }
     
 }  
