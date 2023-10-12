a450d4d4cf3902dbaa58a68b2d3797091f5fc69e
Klaus Dimde <klaus@dimde.info>
2023-10-12 14:10:40 +0200

    Removed obsolete ajax routes

--- src/Routes/web.php
+++ src/Routes/web.php
@@ -12,9 +12,3 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-Route::get('/ajax/searchObjects/{class}/{field}', 'Sunhill\Collection\Controllers\AjaxController@searchObjects');
-Route::get('/ajax/searchObject/{class}', 'Sunhill\Collection\Controllers\AjaxController@searchObject');
-Route::get('/ajax/searchArrayOfString/{class}/{field}', 'Sunhill\Collection\Controllers\AjaxController@getArrayOfStringSuggestion');
-Route::get('/ajax/getClass/{class}', 'Sunhill\Collection\Controllers\AjaxController@getClass');
-Route::get('/ajax/getAttributeType/{name}', 'Sunhill\Collection\Controllers\AjaxController@getAttributeType');
-Route::get('/ajax/searchImportSeries', 'Sunhill\Collection\Controllers\AjaxController@searchImportSeries');
