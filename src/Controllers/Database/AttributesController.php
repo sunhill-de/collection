69db165402e077df729d070e1a014492497e8e0f
Klaus Dimde <klaus@dimde.info>
2023-10-26 21:23:30 +0200

    Implemented attributes as crud

--- src/Controllers/Database/AttributesController.php
+++ src/Controllers/Database/AttributesController.php
@@ -15,66 +15,13 @@ use Sunhill\Collection\Response\Database\Attributes\ShowAttributeResponse;
 use Sunhill\Collection\Response\Database\Attributes\ListChildrenResponse;
 use Sunhill\Collection\Response\Database\Attributes\ListAssociatedObjectsResponse;
 use Sunhill\Collection\Response\Database\Attributes\AttributeDialogResponse;
+use Sunhill\Visual\Controllers\CrudController;
+use Sunhill\Collection\Response\Database\Attributes\AttributesCrudResponse;
 
-class AttributesController extends Controller
+class AttributesController extends CrudController
 {
     
-    public function index()
-    {
-        $response = new SunhillTileViewResponse();
-        return $response->response();
-    }
-    
-    public function list($page = 0)
-    {
-        $response = new ListAttributesResponse();
-        $response->setOffset($page);
-        return $response->response();
-    }
-    
-    public function show($id)
-    {
-        $response = new ShowAttributeResponse();
-        $response->setID($id);
-        return $response->response();
-    }
-    
-    public function add()
-    {
-        $response = new AttributeDialogResponse();
-        $response->setMode('add');
-        return $response->response();
-    }
-    
-    public function execAdd()
-    {
-        $response = new AttributeDialogResponse();
-        $response->setMode('execadd');
-        return $response->response();
-    }
-    
-    public function edit($id)
-    {
-        $response = new AttributeDialogResponse();
-        $response->setID($id);
-        $response->setMode('edit');
-        return $response->response();
-    }
-    
-    public function execEdit($id)
-    {
-        $response = new AttributeDialogResponse();
-        $response->setID($id);
-        $response->setMode('execedit');
-        return $response->response();
-    }
-    
-    public function delete($id)
-    {
-        $response = new DeleteAttributeResponse();
-        $response->setID($id);
-        return $response->response();
-    }
+    protected static $crud_response = AttributesCrudResponse::class;
     
     public function listChildren(int $id, $offset = 0)
     {
--- src/Modules/Database/SunhillFeatureAttributes.php
+++ src/Modules/Database/SunhillFeatureAttributes.php
@@ -2,66 +2,29 @@
 
 namespace Sunhill\Collection\Modules\Database;
 
-use Sunhill\Visual\Response\Database\Classes\ListClassesResponse;
-use Sunhill\Visual\Modules\SunhillModuleBase;
+use Sunhill\Visual\Modules\SunhillCrudModule;
 
-class SunhillFeatureAttributes extends SunhillModuleBase
+class SunhillFeatureAttributes extends SunhillCrudModule
 {
     
-    protected function setupModule()
+    protected static $route_base = 'attributes';
+    
+    protected static $controller = \Sunhill\Collection\Controllers\Database\AttributesController::class;
+    
+    protected function setupBasics()
     {
         $this->setName('Attributes');
-        $this->setDisplayName('Attributes');        
+        $this->setDisplayName('Attributes');
         $this->setDescription('Attributes');
-        $this->addIndex(\Sunhill\Collection\Controllers\Database\AttributesController::class);
-
-        $this->addAction('List')
-            ->addControllerAction([\Sunhill\Collection\Controllers\Database\AttributesController::class, 'list'])
-            ->setVisible(true)
-            ->setRouteAddition('/{page?}')
-            ->setAlias('attributes.list');
-        $this->addAction('Add')
-            ->addControllerAction([\Sunhill\Collection\Controllers\Database\AttributesController::class, 'add'])
-            ->setVisible(true)
-            ->setAlias('attributes.add');
-        $this->addAction('ExecAdd')
-            ->setMethod('post')
-            ->addControllerAction([\Sunhill\Collection\Controllers\Database\AttributesController::class, 'execadd'])
-            ->setVisible(false)
-            ->setAlias('attributes.execadd');
-        $this->addAction('Edit')
-            ->addControllerAction([\Sunhill\Collection\Controllers\Database\AttributesController::class, 'edit'])
-            ->setRouteAddition('/{id}')
-            ->setVisible(false)
-            ->setAlias('attributes.edit');
-        $this->addAction('ExecEdit')
-            ->setMethod('post')
-            ->addControllerAction([\Sunhill\Collection\Controllers\Database\AttributesController::class, 'execedit'])
-            ->setRouteAddition('/{id}')
-            ->setVisible(false)
-            ->setAlias('attributes.execedit');
-        $this->addAction('Show')
-            ->addControllerAction([\Sunhill\Collection\Controllers\Database\AttributesController::class, 'show'])
-            ->setVisible(false)
-            ->setRouteAddition('/{id}')
-            ->setAlias('attributes.show');
-        $this->addAction('Delete')
-            ->addControllerAction([\Sunhill\Collection\Controllers\Database\AttributesController::class, 'delete'])
-            ->setVisible(false)
-            ->setRouteAddition('/{id}')
-            ->setAlias('attributes.delete');
-        $this->addAction('ListChildren')
-            ->addControllerAction([\Sunhill\Collection\Controllers\Database\AttributesController::class, 'listChildren'])
-            ->setVisible(false)
-            ->setRouteAddition('/{id}/{page?}')
-            ->setAlias('attributes.listchildren');
-        $this->addAction('ListObjects')
-            ->addControllerAction([\Sunhill\Collection\Controllers\Database\AttributesController::class, 'listAssociatedObjects'])
-            ->setVisible(false)
-            ->setRouteAddition('/{id}/{page?}')
-            ->setAlias('attributes.listobjects');
-        
     }
     
+    protected function setupAdditional()
+    {
+        $this->addAction('ListObjects')
+             ->addControllerAction([static::$controller, 'listAssociatedObjects'])
+             ->setVisible(false)
+             ->setRouteAddition('/{id}/{page?}')
+             ->setAlias('attributes.listobjects');
+    }
     
 }
