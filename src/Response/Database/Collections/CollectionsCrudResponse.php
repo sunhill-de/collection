eadeb4610a9369299784c11361943d4632970b61
Klaus Dimde <klaus@dimde.info>
2023-11-09 13:04:27 +0100

    Implemented collections crud response

--- src/Response/Database/Collections/CollectionsCrudResponse.php
+++ src/Response/Database/Collections/CollectionsCrudResponse.php
@@ -12,25 +12,17 @@ class CollectionsCrudResponse extends SunhillSemiCrudResponse
 {
     
     protected static $route_base = 'collections';
-        
+
+    protected static $prefix = '{class}/';
+    
     protected function getBasicQuery()
     {
-        return Tags::query();
+        return Collections::query();
     }
     
     protected function getDefaultOrder(): string
     {
         return 'name';
-    }
-    
-    /**
-     * Provides additional links (in this case a link for adding tags)
-     * @return StdClass[]
-     */
-    protected function getAdditionalLinks()
-    {
-        return [
-        ];
     }
     
     /**
@@ -40,6 +32,7 @@ class CollectionsCrudResponse extends SunhillSemiCrudResponse
     protected function getSearchfields()
     {
         return [
+            $this->getStdClass(['value'=>'name','name'=>'name','relations'=>['=','<>','<','<=','>','>=','begins with','ends with','contains']]),
         ];
     }
     
@@ -50,29 +43,19 @@ class CollectionsCrudResponse extends SunhillSemiCrudResponse
      */
     protected function defineList(ListDescriptor &$descriptor)
     {
-        $descriptor->column('name')->title('Name')->searchable('name');
+        $descriptor->column('class')->title('Class name')->searchable('class')->setColumnSortable('class');
+        $descriptor->column('name')->title('Name')->searchable('name')->setColumnSortable('name');
         $descriptor->column('description')->title('Description');
-        $descriptor->column('list')->link('collection.list',['collection'=>'name'])->setLinkTitle('list');
-        $descriptor->column('add')->link('collection.add',['collection'=>'name'])->setLinkTitle('add');
+        $descriptor->column('list')->link('collection.list',['class'=>'name'])->setLinkTitle('list');
+        $descriptor->column('add')->link('collection.add',['class'=>'name'])->setLinkTitle('add');
         $descriptor->column('show')->link('collections.show',['id'=>'name'])->setLinkTitle('show');
     }
     
-    /**
-     * Returns the count of entries for the given filter (if any)
-     * @param string $filter
-     */
-    protected function getEntryCount(): int
+    public function getCollectionsCount(array $conditions = []): int
     {
-        return count(Collections::getRegisteredCollections());
-    }
-    
-    /**
-     * Return the tags that fit to the current filter
-     * @return unknown
-     */
-    protected function getData()
-    {
-        return Collections::getRegisteredCollections();
+        $query = Collections::query();
+        $query = $this->handleCollectionConditions($query, $conditions);
+        return $query->count();        
     }
     
     /**
@@ -82,13 +65,145 @@ class CollectionsCrudResponse extends SunhillSemiCrudResponse
      */
     protected function IDExists($id): bool
     {
+        return !is_null(Collections::searchClass($id));
     }
     
     protected static $entity = 'collection';
     
+    protected function getObjectCount(string $namespace)
+    {
+        return $namespace::search()->count();
+    }
+    
+    protected function getClassInfo($id)
+    {
+        $class = Collections::getNamespaceOfClass($id);
+        
+        return [
+            'caption'=>__("Show class ':classname'", ['classname'=>$id]),
+            'header'=>[
+                __('Key'),
+                __('Value')
+            ],
+            'data'=>[
+                [__('Class name'),$class::getInfo('name')],
+                [__('Description'),$class::getInfo('description')],
+                [__('Namespace'),$class],
+                [__('Table name'),$class::getInfo('table')],
+                [__('editable'),$class::getInfo('editable',false)],
+                [__('instantiable'),$class::getInfo('instantiable',false)],
+                [__('object count'),make_link(route('objects.list',$id),$this->getObjectCount($class))],
+            ],
+            'links'=>[],
+        ];
+    }
+    
+    protected function getType(string $type)
+    {
+        $parts = explode('\\',$type);
+        return substr(array_pop($parts),8);
+    }
+    
+    protected function getClassProperties(string $namespace)
+    {
+        $properties = $namespace::staticPropertyQuery()->get();
+        foreach ($properties as $property) {
+            $property->class = isset($property->owner)?$property->owner::getInfo('name'):'';
+            if (isset($property->description)) {
+                $property->description = __($property->description);
+            } else {
+                $property->description = '';
+            }
+            if (!isset($property->displayable)) {
+                $property->displayable = '';
+            }
+            if (!isset($property->editable)) {
+                $property->editable = '';
+            }
+            if (!isset($property->groupeditable)) {
+                $property->groupeditable = '';
+            }
+            if ($property->default == DefaultNull::class) {
+                $property->default = 'NULL';
+            }
+            $property->semantic = $property->semantic::getName();
+            $property->unit = $property->unit::getName();
+            $property->type = $this->getType($property->type);
+            switch ($property->type) {
+                case 'Varchar':
+                    $property->additional = __('Maximum length: :maxlen', ['maxlen'=>$property->max_len]);
+                    break;
+                case 'Array':
+                case 'Map':
+                    $property->additional = __('Element type: :element',['element'=>$this->getType($property->element_type)]);
+                    break;
+                case 'Object':
+                    $property->additional = __('Allowed class: ');
+                    $first = true;
+                    if (is_array($property->allowed_classes)) {
+                        foreach ($property->allowed_classes as $class) {
+                            $property->additional .= ($first?'':',').$class;
+                            $first = false;
+                        }
+                    } else {
+                        $property->additional .= $property->allowed_classes;
+                    }
+                    break;
+                default:
+                    $property->additional = '';
+            }
+        }
+        return $properties;
+    }
+    
+    protected function getFieldInfo($id)
+    {
+        $result = [
+            'caption'=>__("Show propeties of class ':classname'", ['classname'=>$id]),
+            'header'=>[
+                __('Name'),
+                __('Description'),
+                __('Type'),
+                __('Class'),
+                __('Semantic'),
+                __('Unit'),
+                __('Default'),
+                __('Nullable'),
+                __('Displayable'),
+                __('Editable'),
+                __('Group editable'),
+                __('Additional'),
+            ],
+            'links'=>[]
+        ];
+        
+        $data = $this->getClassProperties(Collections::getNamespaceOfClass($id));
+        $result['data'] = [];
+        foreach ($data as $property) {
+            $result['data'][] = [
+                $property->name,
+                $property->description,
+                $property->type,
+                $property->class,
+                $property->semantic,
+                $property->unit,
+                $property->default,
+                $property->nullable,
+                $property->displayable,
+                $property->editable,
+                $property->groupeditable,
+                $property->additional
+            ];
+        }
+        
+        return $result;
+    }
+    
     protected function getDataSet($id)
     {
         return [
+            'classinfo'=>$this->getClassInfo($id),
+            'fieldinfo'=>$this->getFieldInfo($id),
         ];
     }
     
