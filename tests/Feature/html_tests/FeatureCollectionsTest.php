8f3730e79111f26eec78c9cc0773aefec3e38dc8
Klaus Dimde <klaus@dimde.info>
2023-11-09 13:01:40 +0100

    Corrected tests for semicrud module collection

--- tests/Feature/html_tests/FeatureCollectionsTest.php
+++ tests/Feature/html_tests/FeatureCollectionsTest.php
@@ -2,101 +2,81 @@
 
 namespace Sunhill\Collection\Tests\Feature\html_tests;
 
-use Sunhill\ORM\Facades\Collections;
+use Sunhill\Collection\Tests\DatabaseTestCase;
 
-class FeatureCollectionsTest extends HtmlTestBase
+class FeatureCollectionsTest extends DatabaseTestCase
 {
     
-    /**
-     * @dataProvider CollectionProvider
-     */
-    public function testListCollection($collection, $entries = [])
+    public function testListDefaultPage0()
     {
-        $response = $this->get('/Database/Collections/ListCollection/'.$collection);
+        $response = $this->get('/Database/Collections/List');
+        $response->assertStatus(200);
+        $response->assertSee('Anniversary');
+    }
+    
+    public function testListDefault()
+    {
+        $response = $this->get('/Database/Collections/List/0');
+        $response->assertStatus(200);
+        $response->assertSee('Anniversary');
+    }
+    
+/* There is only one page yet   
+ * public function testListPage()
+    {
+        $response = $this->get('/Database/Collections/List/1');
         $response->assertStatus(200);        
-        foreach ($entries as $key => $value) {
-            $response->assertSee($value);
-        }
+        $response->assertSee('Episode');
+    }
+*/    
+    public function testListLastPage()
+    {
+        $response = $this->get('/Database/Collections/List/-1');
+        $response->assertStatus(200);
+        $response->assertSee('Transaction');
+    }
+    
+    public function testListNoPage()
+    {
+        $response = $this->get('/Database/Collections/List/1000');
+        $response->assertStatus(500);
+        $response->assertSee("is out of range.");
     }
 
-    /**
-     * @dataProvider CollectionProvider
-     */
-    public function testAddCollection($collection)
+    public function testListsOrder()
     {
-        $response = $this->get('/Database/Collections/Add/'.$collection);
-        $response->assertStatus(200);        
-    }
-    
-    /**
-     * @dataProvider CollectionProvider
-     */
-    public function testEditCollection($collection, $entries)
-    {
-        $response = $this->get('/Database/Collections/Edit/'.$collection.'/1');
+        $response = $this->get('/Database/Collections/List/0/name');
         $response->assertStatus(200);
+        $response->assertSee("Event");
     }
     
-    /**
-     * @dataProvider CollectionProvider
-     */
-    public function testShowCollection($collection, $entries)
+    public function testListNoOrder()
     {
-        $response = $this->get('/Database/Collections/Show/'.$collection.'/1');
+        $response = $this->get('/Database/Collections/List/0/nonexisting');
+        $response->assertStatus(500);        
+        $response->assertSee("'nonexisting' is not an allowed order key.");
+    }
+/*    
+    public function testListCombined()
+    {
+        $response = $this->get('/Database/Collections/List/1/name');
         $response->assertStatus(200);
-        foreach ($entries as $key => $value) {
-            $response->assertSee($value);            
-        }
+        $response->assertSee("Person");
     }
-    
-    /**
-     * @dataProvider CollectionProvider
-     */
-    public function testDeleteCollection($collection, $entries, $dataset)
+*/    
+    public function testShow()
     {
-        $table = Collections::searchCollection($collection)::getInfo('table');
-        $this->assertDatabaseHas($table, array_merge(['id'=>1], $dataset));
-        $response = $this->get('/Database/Collections/DeleteCollection/1');
+        $response = $this->get('/Database/Collections/Show/Staff');
         $response->assertStatus(200);
-        $this->assertDatabaseMissing($table, array_merge(['id'=>1], $dataset));
+        $response->assertSee('Staff');
+        $response->assertSee('job');
     }
-    
-    public static function CollectionProvider()
+        
+    public function testShowMissing()
     {
-        return [
-            [
-                'Anniversary',
-                ["Homer's birthday"],
-                ['name'=>"Homer's birthday",'type'=>'birthday','first'=>'1956-05-12'],                
-            ],
- /*          
-            ['Event',['Homer Simpson',['who']]],
-            ['EventType',['name'=>'watch']],
-            ['Genre',['name'=>'fiction']],
-            ['Language',['name'=>'english']],
-            ['MusicalArtist',['name'=>'Muse']],
-            ['Network',['name'=>'home']],
-            ['PersonsRelation',['person1'=>'Homer Simpson','person2'=>'Marge Simpson']],
-            ['ProductGroup',['name'=>'food']],
-            ['Staff',['person'=>'Edward Norton']],
-            ['StaffJob',['name'=>'actor']], */
-        ];    
+        $response = $this->get('/Database/Collections/Show/notexisting');
+        $response->assertStatus(500);
+        $response->assertSee("The ID 'notexisting' is not a valid ID.");
     }
-    
-    public static function HTMLProvider()
-    {
-        return [
- /*           ['/Database/Collections/List',200,'get','Anniversary'],          // Default list classes
-            ['/Database/Collections/Show/Anniversary',200],   // Show class with existing name
-            
-            ['/Database/Collections/Show/NonExistingClass',500],  // Show non existing class
-            ['/Database/Collections/List/1000',500],              // List classes with non existing page
-            ['/Database/Collections/ListCollection/StaffJob',200],
-            ['/Database/Collections/ListCollection/MusicalArtist/0',200],
-            ['/Database/Collections/ListCollection/MusicalArtist/0/id',200],
-            ['/Database/Collections/ListCollection/MusicalArtist/1/id',200],
-            ['/Database/Collections/ListCollection/StaffJob/1000',500],
-   */     ];    
-    }
-    
+        
 }
