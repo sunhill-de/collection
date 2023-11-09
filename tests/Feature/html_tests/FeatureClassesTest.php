d02f370568317ab3f0ca06b436f9e8b1d35f32b8
Klaus Dimde <klaus@dimde.info>
2023-11-09 12:53:56 +0100

    Tests for semi crud respnse 'classes'

--- tests/Feature/html_tests/FeatureClassesTest.php
+++ tests/Feature/html_tests/FeatureClassesTest.php
@@ -2,21 +2,79 @@
 
 namespace Sunhill\Collection\Tests\Feature\html_tests;
 
-class FeatureClassesTest extends HtmlTestBase
+use Sunhill\Collection\Tests\DatabaseTestCase;
+
+class FeatureClassesTest extends DatabaseTestCase
 {
     
-    public static function HTMLProvider()
+    public function testListDefaultPage0()
     {
-        return [
-            ['/Database/Classes/List',200,'get','Address'],          // Default list classes
-            ['/Database/Classes/List/2',200,'get','Network'],        // List classes with page
-            ['/Database/Classes/Show/Person',200],   // Show class with existing name
-            ['/Database/Classes/Show/1',200],        // Show class with existing index            
-            
-            ['/Database/Classes/Show/NonExistingClass',500],  // Show non existing class
-            ['/Database/Classes/Show/1000',500],              // Show class with non existing index
-            ['/Database/Classes/List/1000',500],              // List classes with non existing page
-        ];    
+        $response = $this->get('/Database/Classes/List/');
+        $response->assertStatus(200);
+        $response->assertSee('Address');
     }
     
+    public function testListDefault()
+    {
+        $response = $this->get('/Database/Classes/List/0');
+        $response->assertStatus(200);
+        $response->assertSee('Address');
+    }
+    
+    public function testListPage()
+    {
+        $response = $this->get('/Database/Classes/List/1');
+        $response->assertStatus(200);        
+        $response->assertSee('Episode');
+    }
+    
+    public function testListLastPage()
+    {
+        $response = $this->get('/Database/Classes/List/-1');
+        $response->assertStatus(200);
+        $response->assertSee('WrittenWork');
+    }
+    
+    public function testListNoPage()
+    {
+        $response = $this->get('/Database/Classes/List/1000');
+        $response->assertStatus(500);
+        $response->assertSee("is out of range.");
+    }
+
+    public function testListsOrder()
+    {
+        $response = $this->get('/Database/Classes/List/0/name');
+        $response->assertStatus(200);
+        $response->assertSee("object");
+    }
+    
+    public function testListNoOrder()
+    {
+        $response = $this->get('/Database/Classes/List/0/nonexisting');
+        $response->assertStatus(500);        
+        $response->assertSee("'nonexisting' is not an allowed order key.");
+    }
+    
+    public function testListCombined()
+    {
+        $response = $this->get('/Database/Classes/List/1/name');
+        $response->assertStatus(200);
+        $response->assertSee("Person");
+    }
+    
+    public function testShow()
+    {
+        $response = $this->get('/Database/Classes/Show/Person');
+        $response->assertStatus(200);
+        $response->assertSee('Person');
+    }
+        
+    public function testShowMissing()
+    {
+        $response = $this->get('/Database/Classes/Show/notexisting');
+        $response->assertStatus(500);
+        $response->assertSee("The ID 'notexisting' is not a valid ID.");
+    }
+        
 }
