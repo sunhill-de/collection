6daa4cda8a8e39b87a180118f5c2260bd8ce2f28
Klaus Dimde <klaus@dimde.info>
2023-11-14 15:45:01 +0100

    Display the value on atomar properties

--- src/Response/Ajax/AjaxInfomarketNodes.php
+++ src/Response/Ajax/AjaxInfomarketNodes.php
@@ -10,28 +10,35 @@ use Sunhill\ORM\Facades\InfoMarket;
 class AjaxInfomarketNodes extends AjaxSearchResponse
 {
     
-    protected function assembleSearchResult(string $search)
+    protected function assembleSearchResult(string $search = '')
     {
-        $nodes = InfoMarket::getNodes(($search=="!root!")?"":$search, 'object','anybody');
+        $nodes = InfoMarket::getItem($search, 'anybody', 'object');
         $result = [];
-        foreach ($nodes as $node) {
+        foreach ($nodes->value as $node => $desc) {
             $node_data = [];
-            if ($search == '!root!') {
+            if ($search == '') {
                 $node_data['type'] = 'root';
-                $node_data['id'] = $node->name;
+                $node_data['id'] = $node;
                 $node_data['parent'] = "#";
             } else {
-                $node_data['id'] = $search.'.'.$node->name;
+                $node_data['id'] = $search.'.'.$node;
                 $node_data['parent'] = $search;
             }
-            $node_data['text'] = $node->name;
-            if ($node->semantic == 'Branch') {
-                $node_data["icon"] = "fa fa-folder icon-lg";
-                $node_data["children"] = true;
-            } else {
-                $node_data["icon"] = "fa fa-file fa-large kt-font-default";
-                $node_data["children"] = false;
+            $subnode = InfoMarket::getItem($node_data['id'], 'anybody', 'object');
+            switch ($subnode->type)
+            {
+                case 'array':
+                case 'object':    
+                    $node_data['text'] = $node;
+                    $node_data["icon"] = "fa fa-folder icon-lg";
+                    $node_data["children"] = true;
+                    break;
+                default:    
+                    $node_data['text'] = $node.': <div class="value">'.$subnode->value.'</div>';
+                    $node_data["icon"] = "fa fa-file fa-large kt-font-default";
+                    $node_data["children"] = false;
             }
+
             $result[] = $node_data;
         }
         
