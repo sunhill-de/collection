a1c0e3b135b790800746681f7a2045be19fbfd24
Klaus Dimde <klaus@dimde.info>
2023-11-29 05:27:35 +0100

    Implemented attribute ajax module

--- src/Response/Ajax/AjaxAttributes.php
+++ src/Response/Ajax/AjaxAttributes.php
@@ -2,17 +2,47 @@
 
 namespace Sunhill\Collection\Response\Ajax;
 
-use Sunhill\Visual\Response\Ajax\AjaxResponse;
-use Illuminate\Support\Facades\DB;
-use Sunhill\ORM\Facades\Tags;
+use Sunhill\Visual\Response\Ajax\AjaxSearchResponse;
+use Sunhill\ORM\Facades\Attributes;
 
-class AjaxAttributes extends AjaxResponse
+class AjaxAttributes extends AjaxSearchResponse
 {
     
-    protected function assembleOutput(string $search)
+    protected function assembleGeneralAttributes(string $search)
     {
-        if (!empty($this->parameter1)) {
-            
+        $query = Attributes::query()->get();
+       
+        $result = [];
+        foreach ($query as $attribute) {
+            if (strpos($attribute->name,$search) !== false) {
+                $result[] = $this->makeStdclass(['label'=>$attribute->name,'id'=>$attribute->id]);
+            }
+        }
+        
+        return $result;
+    }
+    
+    protected function assembleClassAttributes(string $search, string $class)
+    {
+        $query = Attributes::query()->get();
+        
+        $result = [];
+        foreach ($query as $attribute) {
+            if ((strpos($attribute->name,$search) !== false) && (strpos($attribute->allowed_classes,$class) !== false)) {
+                $result[] = $this->makeStdclass(['label'=>$attribute->name,'id'=>$attribute->id]);
+            }
+        }
+        
+        return $result;
+        
+    }
+    
+    protected function assembleSearchResult(string $search)
+    {
+        if (empty($this->parameter1)) {
+            return $this->assembleGeneralAttributes($search); 
+        } else {
+            return $this->assembleClassAttributes($search, $this->parameter1);
         }
     }
     
