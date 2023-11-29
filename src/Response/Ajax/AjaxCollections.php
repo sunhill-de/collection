4b713cb0c279ad9a5fe7335c50919bcedd3b8361
Klaus Dimde <klaus@dimde.info>
2023-11-29 05:28:14 +0100

    Implemented collections ajax module

--- src/Response/Ajax/AjaxCollections.php
+++ src/Response/Ajax/AjaxCollections.php
@@ -3,13 +3,21 @@
 namespace Sunhill\Collection\Response\Ajax;
 
 use Sunhill\Visual\Response\Ajax\AjaxSearchResponse;
+use Sunhill\ORM\Facades\Collections;
 
 class AjaxCollections extends AjaxSearchResponse
 {
     
     protected function assembleSearchResult(string $search)
     {
-        
+        $result = [];
+        $classes = Collections::getAllCollections();
+        foreach ($classes as $class => $info) {
+            if (strpos($class,$search) !== false) {
+                $result[] = $this->makeStdclass(['label'=>$class,'id'=>$class]);
+            }
+        }
+        return $result;        
     }
     
 }
